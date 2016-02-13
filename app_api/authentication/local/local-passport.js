8cc33fbc43ba6fcce3081efc44165c38e6115d39
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-02-13 19:23:51 +0100

    Fixed a bug. Now if a user try to register with an already existing email, the system shows a danger alert

--- app_api/authentication/local/local-passport.js
+++ app_api/authentication/local/local-passport.js
@@ -17,30 +17,10 @@ module.exports = function (userRef, passportRef) {
         if (!user || !user.validPassword(password)) {
           return done(null, false, 'Incorrect username or password.');
         }
-
-        //if (user) {
-        // return done(null, false, 'Email already taken!');
-        //} else {
-          return done(null, user);
-        //}
-      //     // if there is no user with that email
-      //     // create the user
-      //     var newUser = new userRef();
-
-      //     // set the user's local credentials
-      //     newUser.local.email    = email;
-      //     newUser.local.password = newUser.setPassword(password);
-
-      //     // save the user
-      //     newUser.save(function(err) {
-      //       if (err)
-      //         throw err;
-      //       return done(null, newUser);
-      //     });
-      //   }
+        return done(null, user);
       });
-  });
-}));
+    });
+  }));
 
-return module;
+  return module;
 }
--- app_api/controllers/auth-local.js
+++ app_api/controllers/auth-local.js
@@ -7,31 +7,38 @@ var utils = new Utils();
 
 module.exports.register = function(req, res) {
   if(!req.body.name || !req.body.email || !req.body.password) {
-    utils.sendJSONresponse(res, 400, {
-      "message": "All fields required"
-    });
+    utils.sendJSONresponse(res, 400, "All fields required");
     return;
   }
 
-  var user = new User();
-  user.local.name = req.body.name;
-  user.local.email = req.body.email;
-  user.setPassword(req.body.password);
-
-  user.save(function(err, savedUser) {
-    var token;
-    if (err) {
-      utils.sendJSONresponse(res, 404, err);
-    } else {
-      token = user.generateJwt();
-      utils.sendJSONresponse(res, 200, 
-        JSON.stringify({ 
-          'token' : token,
-          'id' : savedUser._id
-        })
-      );
+  User.findOne({ 'local.email': req.body.email }, function (err, user) {
+    if (err || user) { 
+      utils.sendJSONresponse(res, 400, "User already exists. Try to login.");
+      return;
     }
+
+    var user = new User();
+    user.local.name = req.body.name;
+    user.local.email = req.body.email;
+    user.setPassword(req.body.password);
+
+    user.save(function(err, savedUser) {
+      var token;
+      if (err) {
+        utils.sendJSONresponse(res, 404, err);
+      } else {
+        token = user.generateJwt();
+        utils.sendJSONresponse(res, 200, 
+          JSON.stringify({ 
+            'token' : token,
+            'id' : savedUser._id
+          })
+          );
+      }
+    });
   });
+
+  
 };
 
 module.exports.login = function(req, res) {
@@ -55,7 +62,7 @@ module.exports.login = function(req, res) {
           'token' : token,
           'id' : user._id
         })
-      );
+        );
     } else {
       utils.sendJSONresponse(res, 401, info);
     }
