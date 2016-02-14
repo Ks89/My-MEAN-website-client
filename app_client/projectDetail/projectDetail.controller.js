7c05650b94cbc685a56edd0ec3ae76d7d474da53
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-02-14 23:08:58 +0100

    trying to fix project details menu on the right

--- app_client/projectDetail/projectDetail.controller.js
+++ app_client/projectDetail/projectDetail.controller.js
@@ -1,35 +1,46 @@
 (function () {
 
   angular
-    .module('mySiteApp')
-    .controller('projectDetailCtrl', projectDetailCtrl);
+  .module('mySiteApp')
+  .controller('projectDetailCtrl', projectDetailCtrl);
 
-  projectDetailCtrl.$inject = ['$location', '$routeParams', 'projectsData'];
-  function projectDetailCtrl ($location, $routeParams, projectsData) {
+  projectDetailCtrl.$inject = ['$location', '$routeParams', '$anchorScroll', 'projectsData'];
+  function projectDetailCtrl ($location, $routeParams, $anchorScroll, projectsData) {
     var vm = this;
     vm.projectid = $routeParams.projectid;
 
     vm.images = []; 
 
-    vm.scrollTo = function (destination) {
-      return $location.path() + '#' + destination;
-    };
+    // vm.scrollTo = function (destination) {
+    //   return $location.path() + '#' + destination;
+    // };
+
+    vm.scrollTo = function(id) {
+         var old = $location.hash();
+    $location.hash(id);
+    $anchorScroll();
+    //reset to old to keep any additional routing logic from kicking in
+    $location.hash(old);
+    
+      // $location.hash(id);
+      // $anchorScroll();
+    }
 
     projectsData.projectById(vm.projectid)
-      .success(function(data) {
-        vm.data = { project: data };
-        vm.pageHeader = {
-          title: vm.data.project.name
-        };
+    .success(function(data) {
+      vm.data = { project: data };
+      vm.pageHeader = {
+        title: vm.data.project.name
+      };
 
-        data.gallery.forEach(function (element, index) {
-            vm.images.push(element);
-        });
-
-        console.log(vm.data);
-      })
-      .error(function (e) {
-        console.log(e);
+      data.gallery.forEach(function (element, index) {
+        vm.images.push(element);
       });
-    };
+
+      console.log(vm.data);
+    })
+    .error(function (e) {
+      console.log(e);
+    });
+  };
 })();
