d44c87838ec14e96fa507cf8cdcd45dbdec11e08
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-05-11 00:13:23 +0200

    unlink service code refactoring in auth-common using async.waterfall

--- app_api/controllers/auth-common.js
+++ app_api/controllers/auth-common.js
@@ -4,7 +4,7 @@ var logger = require('../utils/logger.js');
 var jwt = require('jsonwebtoken');
 var Utils = require('../utils/util.js');
 var utils = new Utils();
-
+var async = require('async');
 
 /* GET to decode a JWT passing the token itself*/
 /* /api/decodeToken/:token */
@@ -120,39 +120,47 @@ var unlinkServiceByName = function(req, serviceName, res) {
     utils.sendJSONresponse(res, 404, null);
   }
 
-  // verify a token symmetric
-  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-    if(err) {
-      console.error("Unknown error during jwt.verify");
-      utils.sendJSONresponse(res, 404, null);
-    } 
-    console.log('Trying to decode jwt');
-    if(!decoded) {
-      console.error("Impossible to decode: " + decoded);
-      utils.sendJSONresponse(res, 404, null);
-    }
-    console.log("decoded valid");
-    if(!utils.isJwtValidDate(decoded)) {
-      console.error('No data valid');
-      utils.sendJSONresponse(res, 404, null);
-    }
-    console.log("SystemDate valid");
+  async.waterfall([
+    done => {
+      jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+        if (err) throw err;
+        
+        console.log('Trying to decode jwt');
+        if(!decoded) {
+          console.error("Impossible to decode: " + decoded);
+          throw err;
+        }
+        console.log("decoded valid");
+        if(!utils.isJwtValidDate(decoded)) {
+          console.error('No data valid');
+          throw err;
+        }
+        console.log("SystemDate valid");
 
-    var user = decoded.user;
-    console.log("User is: ");
-    console.log(user);
-
-    User.findById(user._id, function(err, user) {
-      if (err) { 
-        console.error('Error user not found (usersReadOneById)' + err);
-        utils.sendJSONresponse(res, 404, null);
-      }
-      if(!user) {
-        console.error("User not found - cannot unlink (usersReadOneById)");
-        utils.sendJSONresponse(res, 404, null);
-      }
-      // if the user is found, then log them in
-      console.log("User found (usersReadOneById): " + user);
+        var user = decoded.user;
+        console.log("User is: ");
+        console.log(user);
+        done(err, decoded, user);
+      });
+    }, 
+    (token, user, done) => {
+      User.findById(user._id, function(err, user) {
+        if (err) { 
+          console.error('Error user not found (usersReadOneById)' + err);
+          throw err;
+          //utils.sendJSONresponse(res, 404, null);
+        }
+        if(!user) {
+          console.error("User not found - cannot unlink (usersReadOneById)");
+          throw err;
+          //utils.sendJSONresponse(res, 404, null);
+        }
+        // if the user is found, then log them in
+        console.log("User found (usersReadOneById): " + user);
+        done(err, user, token);
+      });
+    },
+    (user, token, done) => {
       var lastUnlink = checkIfLastUnlink(serviceName, user);
       console.log('Check if last unlink: ' + lastUnlink);
       if(lastUnlink) {
@@ -165,23 +173,34 @@ var unlinkServiceByName = function(req, serviceName, res) {
             console.log('Last unlink, session data destroyed');
           });
         }
-        utils.sendJSONresponse(res, 200, {});
+        done(null, user);
+        //utils.sendJSONresponse(res, 200, {});
       } else {
         console.log("Unlinking normal situation, without a remove....");
         user = removeServiceFromDb(serviceName, user);
         user.save(function(err) {
           if(err) {
             console.error("Impossible to remove userService from db");
-            utils.sendJSONresponse(res, 404, null);
+            throw err;
+            //utils.sendJSONresponse(res, 404, null);
           }
           
           req.session.authToken = generateJwtCookie(user);
           console.log("Unlinking, regenerate session token after unlink");
-          utils.sendJSONresponse(res, 200, user);
+          //utils.sendJSONresponse(res, 200, user);
+          done(err, user);
         });
       }
+    }], (err, user) => {
+      console.log(err);
+      if (err) { 
+        console.log(err);
+        utils.sendJSONresponse(res, 404, null);
+        //return next(err);
+      } else {
+        utils.sendJSONresponse(res, 200, "User unlinked correctly!");      
+      }
     });
-  });
 };
 
 module.exports = {
