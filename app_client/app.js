245d5aa9698d16cc30147d73ab0d8d1d3bb27471
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-05-13 00:26:56 +0200

    first working impl of routeprovider with resolve on profile, to access to profile only if you are logged in. Also removed a useless factory.

--- app_client/app.js
+++ app_client/app.js
@@ -12,39 +12,39 @@
 				controllerAs: 'vm'
 			})
 			.when('/projects', {
-        		templateUrl: '/projectList/projectList.view.html',
-		        controller: 'projectListCtrl',
-		        controllerAs: 'vm'
-		    })
+				templateUrl: '/projectList/projectList.view.html',
+				controller: 'projectListCtrl',
+				controllerAs: 'vm'
+			})
 			.when('/projects/:projectid', {
 				templateUrl: '/projectDetail/projectDetail.view.html',
 				controller: 'projectDetailCtrl',
 				controllerAs: 'vm'
 			})
 			.when('/cv', {
-        		templateUrl: '/cv/cv.view.html',
-		        controller: 'cvCtrl',
-		        controllerAs: 'vm'
+				templateUrl: '/cv/cv.view.html',
+				controller: 'cvCtrl',
+				controllerAs: 'vm'
 			})
 			.when('/contact', {
-        		templateUrl: '/contact/contact.view.html',
-		        controller: 'contactCtrl',
-		        controllerAs: 'vm'
+				templateUrl: '/contact/contact.view.html',
+				controller: 'contactCtrl',
+				controllerAs: 'vm'
 			})
 			.when('/about', {
-        		templateUrl: '/common/views/genericText.view.html',
-		        controller: 'aboutCtrl',
-		        controllerAs: 'vm'
+				templateUrl: '/common/views/genericText.view.html',
+				controller: 'aboutCtrl',
+				controllerAs: 'vm'
 			})
 			.when('/register', {
-          		templateUrl: '/auth/register/register.view.html',
-          		controller: 'registerCtrl',
-          		controllerAs: 'vm'
+				templateUrl: '/auth/register/register.view.html',
+				controller: 'registerCtrl',
+				controllerAs: 'vm'
 			})
 			.when('/login', {
-          		templateUrl: '/auth/login/login.view.html',
-          		controller: 'loginCtrl',
-          		controllerAs: 'vm'
+				templateUrl: '/auth/login/login.view.html',
+				controller: 'loginCtrl',
+				controllerAs: 'vm'
 			})
 			.when('/reset/:emailToken', {
 				templateUrl: '/auth/resetPassword/resetPassword.view.html',
@@ -65,43 +65,40 @@
 			//You can call /profile and also /profile/uhfffg
 			//using the same route
 			.when('/profile/:token?', { 
-          		templateUrl: '/profile/profile.view.html',
-          		controller: 'profileCtrl',
-          		controllerAs: 'vm'
+				templateUrl: '/profile/profile.view.html',
+				controller: 'profileCtrl',
+				controllerAs: 'vm', 
+				resolve: {
+					returnedData: ['$q', '$location', 'authentication', function ($q, $location, authentication) {
+						authentication.isLoggedIn()
+					    .then(function(result) {
+					      console.log('Profile resolve ------------------------ SUCCESS: ' + result);
+					      if(!result) {
+					      	$location.path('/login');
+					      }
+					      //vm.isLoggedIn =  result;
+					    },function(reason) {
+					      console.log('Profile resolve ------------------------ ERROR: ' + reason);
+					      $location.path('/login');
+					    });
+				    }]
+	      		}
+				
 			})
-      		.otherwise({redirectTo: '/'});
-		
+			.otherwise({redirectTo: '/'});
+
 		$locationProvider.html5Mode({
 			enabled: true,
 			requireBase: true,
 			rewriteLinks: false
 		});
 
-		$httpProvider.interceptors.push('authInterceptor'); //inject my authInterceptor.factory.js
+		//configura XSRF/CSRF
+		// $httpProvider.defaults.xsrfCookieName = 'XSRF-TOKEN';
+  		// $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 	}
 
 	angular
-		.module('mySiteApp')
-		.config(['$routeProvider', '$locationProvider', '$httpProvider', config]);
-
+	.module('mySiteApp')
+	.config(['$routeProvider', '$locationProvider', '$httpProvider', config]);
 })();
-
-
-// .when('/profile/:token?', { 
-//           		templateUrl: '/profile/profile.view.html',
-//           		controller: 'profileCtrl',
-//           		controllerAs: 'vm',
-//           		resolve: {
-// 				    resolvedAuth: function( $q, authentication ) {
-// 				        var deferred = $q.defer();
-// 				        deferred.resolve({
-// 				        	isLogged: function( ) {
-// 				            	return authentication.isLoggedIn();
-// 				            }
-// 				        });
-// 				        return deferred.promise;
-// 				    }
-//           		}
-//           		// ,
-//           		// resolveAs: 'resolve'
-// 			})
