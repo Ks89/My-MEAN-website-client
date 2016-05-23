f76d29e0f3e13ef35cc95353de7a5014a5ac0730
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-05-23 22:32:40 +0200

    code refactoring collapse-db

--- app_api/controllers/auth-experimental-collapse-db.js
+++ app_api/controllers/auth-experimental-collapse-db.js
@@ -7,29 +7,25 @@ var Utils = require('../utils/util.js');
 var async = require('async');
 
 
-module.exports.collapseDb = (userref, serviceName) => {
+module.exports.collapseDb = (loggedUser, serviceName) => {
 
-	if(!userref) {
-		console.err("impossibile to collapseDb becase user is null or undefined");
+	if(!loggedUser || !serviceName) {
+		console.err("impossibile to collapseDb becase either loggedUser or serviceName are null or undefined.");
 	}
 
-	var extUser = Object.create(userref);
-
 	console.log("--------------------------******----");
-	console.log("--------------------------******----");
-	console.log("--------------------------******----");
-	console.log('called collapseDb');
-	console.log("INPUT extUser");
-	console.log(extUser);
+	console.log("INPUT loggedUser");
+	console.log(loggedUser);
 	console.log("serviceName: " + serviceName);
 
-	const inputEmail = extUser[serviceName] ? extUser[serviceName].email : null;
+	const inputEmail = loggedUser[serviceName] ? loggedUser[serviceName].email : null;
 	
-
-	console.log("1*****************************************");
-	console.log(extUser);
-
 	console.log("inputEmail " + inputEmail);
+
+	if(!inputEmail) {
+		console.err("inputEmail is not valid");
+		return;
+	}
 
 	console.log("--------------------------******----");
 	console.log("--------------------------******----");
@@ -41,129 +37,118 @@ module.exports.collapseDb = (userref, serviceName) => {
 	query[key] = inputEmail;
 
 	// query._id = {
-	// 	'$ne' : extUser._id
+	// 	'$ne' : loggedUser._id
 	// };
-
-	console.log("1bis*****************************************");
-	console.log(extUser);
 
 	console.log(query);
 
-	if(inputEmail) {
-		console.log("1tris*****************************************");
-			console.log(extUser);
+	const idLoggedUser = loggedUser._id + '';
 
-		const iduserOriginal = extUser._id + '';
+	User.find(query, (err, users) => {
 
+		if(err) {
+			console.err("--------------------------******---- Error - users not found!!!");
+			throw err;
+		}
 
+		console.log("--------------------------******---- users found");
+		console.log(users);
 
-		User.find(query, (err, users) => {
+		var user = users.find(function (el) { 
+			if(el && el._id) {
+				return el._id + '' === idLoggedUser + '';
+			}	
+		});
 
-			if(err) {
-				console.log("--------------------------******---- Error - user not found!!!");
-			}
+		if(!user) {
+			console.err("--------------------------******---- Error - user not found!!!");	
+			return;		
+		}
 
-			console.log("--------------------------******---- search");
+		let duplicatedUser = {};
 
-			console.log("--------------------------******---- users found");
-			console.log(users);
-			console.log("--------------------------******---- starting cycle");
+		console.log("2*****************************************");
+		console.log(user);
+		console.log("2*****************************************");
+		const idUser = user._id + '';
 
+		for(let dbUser of users) {
+			console.log("--------------------------******---- dbUser in for: ");
+			console.log(dbUser);
+			console.log("--------------------------******---- if condition");
+			console.log("--------------------------******----dbUser[serviceName]: ");
+			console.log((dbUser ? dbUser[serviceName] : 'not found'));
 
-			var user = users.find(function (el) { 
-				if(el && el._id) {
-					console.log("---" + el);
-    			return el._id + '' === iduserOriginal + '';
-    		}	
-			});
+			const iddbUser = dbUser._id + '';
 
-			let duplicatedUser = {};
-
-			console.log("2*****************************************");
-			console.log(user);
-			console.log("2*****************************************");
-
-			for(let iUser of users) {
-				console.log("--------------------------******---- iUser in for: ");
-				console.log(iUser);
-				console.log("--------------------------******---- if condition");
-				console.log("--------------------------******----iUser[serviceName]: ");
-				console.log((iUser ? iUser[serviceName] : 'not found'));
-
-				const idiUser = iUser._id + '';
-				const iduser = user._id + '';
-				console.log("--------------------------******----iUser._id: " + iUser._id);
-				console.log("--------------------------******----user._id: " + user._id);
-				if (iUser && iUser[serviceName] && iUser[serviceName].email === inputEmail && idiUser !== iduser) {
-					console.log('--------------------------******----found a duplicated ' + serviceName + ' iUser:');
-					console.log(iUser[serviceName]);
-					duplicatedUser = iUser;
-					break;
+			console.log("--------------------------******----dbUser._id: " + iddbUser);
+			console.log("--------------------------******----user._id: " + idUser);
+			if (dbUser && dbUser[serviceName] && dbUser[serviceName].email === inputEmail && iddbUser !== idUser) {
+				console.log('--------------------------******----found a duplicated ' + serviceName + ' dbUser:');
+				console.log(dbUser[serviceName]);
+				duplicatedUser = dbUser;
+				break;
 					//here I found one account with the same service
 					//I want to merge all infos in this account [not logged] with the new one [used to connect] (to prevent logout/login and session invalidation)
-				} 
-
-				console.log("3*****************************************");
-				console.log(user);
-				console.log("3*****************************************");
+				}
 				console.log('--------------------------******----cycle completed');
-			}
+		}
 
-			console.log("**--------------------------******----preparing to collapse duplicated db's users");
-			console.log(user);
-			console.log("**--------------------------******----");
+		console.log("**--------------------------******----preparing to collapse duplicated db's users");
+		console.log(user);
+		console.log("**--------------------------******----");
 
-			let updated = false;
+		let updated = false;
 
-			for(let s of ['google', 'github', 'facebook', 'local', 'linkedin', 'twitter']) {
-				console.log('**--------------------------******----cycle s: ' + s + ', serviceName: ' + serviceName);
-				if(s !== serviceName) {
-					console.log('--------------------------******----user[s]: ' + user[s]);
-					console.log('--------------------------******----duplicatedUser[s]: ' + duplicatedUser[s]);
-					console.log('--------------------------******----!user[s]: ' + !user[s]);
-					console.log('--------------------------******----(!user[s] || !user[s].email): ' + (!user[s] || !user[s].email));
-					if((!user[s] || !user[s].email) && duplicatedUser[s]) {
-						console.log("**--------------------------******---- merging service: " + s);
-						console.log("**--------------------------******----  modified user in cycle (duplicatedUser): ");
-						console.log(duplicatedUser[s]);
-						console.log("**--------------------------******----");
-						console.log("**--------------------------******----  user in cycle before changes: ");
-						console.log(user[s]);
-						console.log("**--------------------------******----");
+		for(let s of ['google', 'github', 'facebook', 'local', 'linkedin', 'twitter']) {
+			console.log('**--------------------------******----cycle s: ' + s + ', serviceName: ' + serviceName);
+			if(s !== serviceName) {
+				// console.log('--------------------------******----user[s]: ' + user[s]);
+				// console.log('--------------------------******----duplicatedUser[s]: ' + duplicatedUser[s]);
+				// console.log('--------------------------******----!user[s]: ' + !user[s]);
+				// console.log('--------------------------******----(!user[s] || !user[s].email): ' + (!user[s] || !user[s].email));
+				if((!user[s] || !user[s].email) && duplicatedUser[s]) {
+					console.log("**--------------------------******---- merging service: " + s);
+					// console.log("**--------------------------******----  modified user in cycle (duplicatedUser): ");
+					// console.log(duplicatedUser[s]);
+					// console.log("**--------------------------******----");
+					// console.log("**--------------------------******----  user in cycle before changes: ");
+					// console.log(user[s]);
+					// console.log("**--------------------------******----");
 
-						user[s] = duplicatedUser[s];
-						updated=true;
-						console.log("**--------------------------******----  modified user in cycle: ");
-						console.log(user[s]);
-						console.log("**--------------------------******----");
-					}
+					user[s] = duplicatedUser[s];
+					updated=true;
 				}
 			}
+		}
 
-			console.log("**--------------------------******---- modified user");
-			console.log(user);
-			console.log("**--------------------------******---- saving this modified user");
+		console.log("**--------------------------******---- modified user");
+		console.log(user);
+		console.log("**--------------------------******---- saving this modified user");
 
-			if(duplicatedUser && updated) {
-				user.save((err, savedUser) => {
-		      if (err) {
-		      	console.log("Error while saving collapsed users");
-		        throw err;
-		      }
-		      console.log("Saved modified user: " + savedUser); 
-		  	});
+		if(duplicatedUser && updated) {
+			user.save((err, savedUser) => {
+				if (err) {
+					console.log("Error while saving collapsed users");
+					throw err;
+				}
+				console.log("Saved modified user: " + savedUser); 
+			});
 
-		  	console.log("--------------------------******---- removing duplicated user");
+			console.log("--------------------------******---- removing duplicated user [OK]");
 
-		  	User.findByIdAndRemove(duplicatedUser._id, function(err) {
-				  if (err) throw err;
+			User.findByIdAndRemove(duplicatedUser._id, err => {
+				if (err) {
+					throw err;
+				}
 
-				  // we have deleted the user
-				  console.log('--------------------------******---- duplicated User deleted!');
-				});
-	  	}
+			  // we have deleted the user
+			  console.log('--------------------------******---- duplicated User deleted! [OK]');
+			});
+		} else {
+			console.log("I can't do anything because there isn't a duplicated users! [OK]");
+		}
 
-			console.log("--------------------------******---- search finished");
-		});
-	}
+		console.log("--------------------------******---- search finished");
+	});
 };
