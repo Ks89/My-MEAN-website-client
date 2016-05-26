4936bd0fff0ffc01e88cac256be7554d35a6a666
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-05-26 22:50:26 +0200

    code refactoring

--- app_client/projectList/projectList.controller.js
+++ app_client/projectList/projectList.controller.js
@@ -4,8 +4,8 @@
   .module('mySiteApp')
   .controller('projectListCtrl', projectListCtrl);
 
-  projectListCtrl.$inject = ['$scope', 'projectsData'];
-  function projectListCtrl ($scope, projectsData) {
+  projectListCtrl.$inject = ['projectsData'];
+  function projectListCtrl (projectsData) {
     var vm = this;
     
     vm.pageHeader = {
@@ -17,70 +17,67 @@
     vm.sidebarTitle = "What can you do?";
     vm.sidebar = {
       timeline: [
-        { 
-          title: "Discover",
-          body: "Check my projects on GitHub.",
-          icon: "search",
-          color: "badge"
-        },
-        { 
-          title: "Like",
-          body: "Star the project that you like.",
-          icon: "star",
-          color: "danger"
-        },
-        { 
-          title: "Improve",
-          body: "Fork a project to enhance it.",
-          icon: "plus",
-          color: "warning"
-        },
-        { 
-          title: "Collaborate",
-          body: "Create a pull request with your improvements.",
-          icon: "user",
-          color: "info"
-        },
-        { 
-          title: "Share",
-          body: "Share the project with your friends or on the web.",
-          icon: "globe",
-          color: "success"
-        }
-    ]};
+      { 
+        title: "Discover",
+        body: "Check my projects on GitHub.",
+        icon: "search",
+        color: "badge"
+      },
+      { 
+        title: "Like",
+        body: "Star the project that you like.",
+        icon: "star",
+        color: "danger"
+      },
+      { 
+        title: "Improve",
+        body: "Fork a project to enhance it.",
+        icon: "plus",
+        color: "warning"
+      },
+      { 
+        title: "Collaborate",
+        body: "Create a pull request with your improvements.",
+        icon: "user",
+        color: "info"
+      },
+      { 
+        title: "Share",
+        body: "Share the project with your friends or on the web.",
+        icon: "globe",
+        color: "success"
+      }
+      ]};
 
-    vm.message = "Searching for projects";
-    
-    projectsData.projectList()
-    .success(function(data) {
-      console.log(data);
-      vm.message = data.length > 0 ? "" : "No projects found";
+      vm.message = "Searching for projects";
       
-      vm.data = { projects: data };
-      
-      var ellips = "...";
-      var subStr = "";
-      var maxLength = 100;
+      projectsData.projectList()
+      .success(function(data) {
+        console.log(data);
+        vm.message = data.length > 0 ? "" : "No projects found";
 
-      data.forEach(function(currentValue, index, array) {
-        subStr = currentValue.shortDescription.substring(0,maxLength);
-        if(subStr.length >= maxLength) {
-          subStr = subStr.concat(ellips);
+        //vm.projects = data;
+        if(data && data.length > 0) {
+          const ellips = "...";
+          var subStr = "";
+          const maxLength = 100;
+
+          vm.projects = data.map(function(project, index, array) {
+            subStr = project.shortDescription.substring(0,maxLength);
+            if(subStr.length >= maxLength) {
+              subStr = subStr + ellips;
+            }
+            //update all shortDescriptions
+            project.shortDescription = subStr;
+            console.log("map shortDescription: " + project.shortDescription);
+            return project;
+          });
         }
-        //update all shortDescriptions
-        vm.data.projects[index].shortDescription = subStr;
+      })
+      .error(function (e) {
+        console.log(e);
+        vm.message = "Sorry, something's gone wrong, please try again later";
       });
-    })
-    .error(function (e) {
-      console.log(e);
-      vm.message = "Sorry, something's gone wrong, please try again later";
-    });
+    }
 
-    vm.showError = function (error) {
-      $scope.$apply(function() {
-        vm.message = error.message;
-      });
-    };
-  }
-
-})();
+  })();
