e3851dacc800aa1a4e4846e3b3332c1d7ea8f3d2
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-06-04 15:13:29 +0200

    contact's view updated with a new layout and ladda's button

--- app_client/contact/contact.controller.js
+++ app_client/contact/contact.controller.js
@@ -13,33 +13,41 @@
 		};		
 		vm.message = "Contact page";
 
+		vm.isWaiting = false;
+		vm.formDisable = false;
+
 		vm.publicKey = "6Lf0jxQTAAAAAIDxhvAqGseKy_KV2_4iViVeQWYi"; //not secret, no problem ;)
 
 		vm.onSubmit = function () {
 			console.log(vm.formData);
+			vm.isWaiting = true;
+      vm.formError = "";
 
-	        var response = $window.grecaptcha.getResponse("");
+      var response = $window.grecaptcha.getResponse("");
 
-	        console.log('Response is: ' + response);
+      console.log('Response is: ' + response);
 
-	        // Build the post data with the emailFormData and the response string
-	        var data = {
-	            response: response,
-	            emailFormData: vm.formData
-	        };
+      // Build the post data with the emailFormData and the response string
+      var data = {
+          response: response,
+          emailFormData: vm.formData
+      };
 
-	        //send the form with the captcha
-	        //if the result is "succes" display a message
-	        //otherwise print an error
-	        contactData.sendTheFormWithCaptcha(data)
-	            .success(function (result) {
-	                vm.formResultMessage = "Success! Captcha is valid, thank you.";
-	                vm.error = "success";
-	            })
-	            .error(function (result) {
-	                vm.formResultMessage = "Invalid Captcha!";
-	                vm.error = "danger";
-	            });
+      //send the form with the captcha
+      //if the result is "succes" display a message
+      //otherwise print an error
+      contactData.sendTheFormWithCaptcha(data)
+      .success(function (result) {
+        vm.formResultMessage = "Success! Captcha is valid, thank you.";
+        vm.error = "success";
+        vm.isWaiting = false;
+        vm.formDisable = true;
+      })
+      .error(function (result) {
+        vm.formResultMessage = "Invalid Captcha!";
+        vm.error = "danger";
+        vm.isWaiting = false;
+      });
 		};
 	}
 })();
