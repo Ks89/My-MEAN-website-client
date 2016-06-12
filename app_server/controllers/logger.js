a090b13160e321c2277a019fc9912d0293e27f37
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-06-12 23:55:10 +0200

    updated logger

--- app_server/controllers/logger.js
+++ app_server/controllers/logger.js
@@ -19,8 +19,9 @@ module.exports.exception = (req, res) => {
 	console.error("Called log-exception = ");
 	console.error(req.body);
 	logger.error('Called log-exception: ' + JSON.stringify(req.body));
+
 	if(req.body) {
-		Utils.sendJSONresponse(res, 200, "Exception logged on server: " + req.body);
+		Utils.sendJSONresponse(res, 200, "Exception logged on server: " + JSON.stringify(req.body));
 	} else {
 		Utils.sendJSONresponse(res, 404, "Impossible to log exception on server, body is empty");
 	}
@@ -30,8 +31,14 @@ function log(req, res, type) {
 	console.log('Called log-' + type + ' = ');
 	console.log(req.body);
 	logger.error('Called log-' + type + ': ' + JSON.stringify(req.body.message));
+
+	var response = {
+		message: type + " logged on server",
+		body : req.body
+	};
+
 	if(req.body) {
-		Utils.sendJSONresponse(res, 200, type + "logged on server: " + req.body);
+		Utils.sendJSONresponse(res, 200, response);
 	} else {
 		Utils.sendJSONresponse(res, 404, "Impossible to log" + type + " on server, body is empty");
 	}
