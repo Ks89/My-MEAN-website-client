0025b7dd0012e0d15b763972adf224d294358b42
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-06-16 23:59:51 +0200

    refactoring

--- test-server-integration/users-test.js
+++ test-server-integration/users-test.js
@@ -24,7 +24,7 @@ describe('users', () => {
 			user.setPassword('Password1');
 			user.local.activateAccountToken = 'TOKEN';
       user.local.activateAccountExpires =  new Date(Date.now() + 24*3600*1000); // 1 hour
-			user.local.resetPasswordToken = 'TOKEN';
+      user.local.resetPasswordToken = 'TOKEN';
       user.local.resetPasswordExpires = Date.now() + 3600000; // 1 hour
       user.github.id = '1231232';
       user.github.token = 'TOKEN';
@@ -33,58 +33,58 @@ describe('users', () => {
       user.github.username = 'username';
       user.github.profileUrl = 'http://fakeprofileurl.com/myprofile';
       user.profile = {
-				name : 'username',
-		    surname : 'username',
-		    nickname : 'username',
-		    email : 'email@email.it',
-		    updated : new Date(),
-		    visible : true
+      	name : 'username',
+      	surname : 'username',
+      	nickname : 'username',
+      	email : 'email@email.it',
+      	updated : new Date(),
+      	visible : true
       }
 
-		  user.save((err, prj) => {
-		  	user._id = prj._id;
-		  	done(err);
-		  });
-		});
+      user.save((err, prj) => {
+      	user._id = prj._id;
+      	done(err);
+      });
+  });
 
 		it('should correctly get a single user by its id', done => {
 			agent
 			.get('/api/users/' + user._id)
-	    .set('Content-Type', 'application/json')
-	    .set('Accept', 'application/json')
-	    .expect(200)
-	    .end((err, res) => {
-	    	if (err) {
-	    		return done(err);
-	    	} else {
-	    		let usr = res.body;
-	    		expect(usr).to.be.not.null;
-	    		expect(usr).to.be.not.undefined;
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				} else {
+					let usr = res.body;
+					expect(usr).to.be.not.null;
+					expect(usr).to.be.not.undefined;
 
-	    		expect(usr.local.name).to.be.equals(user.local.name);
+					expect(usr.local.name).to.be.equals(user.local.name);
 					expect(usr.local.email).to.be.equals(user.local.email);
 					expect(usr.local.hash).to.be.equals(user.local.hash);
 					expect(usr.local.activateAccountToken).to.be.equals(user.local.activateAccountToken);
 		   //    expect(usr.local.activateAccountExpires).to.be.equals(user.local.activateAccountExpires);
-					expect(usr.local.resetPasswordToken).to.be.equals(user.local.resetPasswordToken);
+		   expect(usr.local.resetPasswordToken).to.be.equals(user.local.resetPasswordToken);
 		   //    expect(usr.local.resetPasswordExpires).to.be.equals(user.local.resetPasswordExpires);
-		      expect(usr.github.id).to.be.equals(user.github.id);
-		      expect(usr.github.token).to.be.equals(user.github.token);
-		      expect(usr.github.email).to.be.equals(user.github.email);
-		      expect(usr.github.name).to.be.equals(user.github.name);
-		      expect(usr.github.username).to.be.equals(user.github.username);
-		      expect(usr.github.profileUrl).to.be.equals(user.github.profileUrl);
-					expect(usr.profile.name).to.be.equals(user.profile.name);
-				  expect(usr.profile.surname).to.be.equals(user.profile.surname);
-				  expect(usr.profile.nickname).to.be.equals(user.profile.nickname);
-				  expect(usr.profile.email).to.be.equals(user.profile.email);
+		   expect(usr.github.id).to.be.equals(user.github.id);
+		   expect(usr.github.token).to.be.equals(user.github.token);
+		   expect(usr.github.email).to.be.equals(user.github.email);
+		   expect(usr.github.name).to.be.equals(user.github.name);
+		   expect(usr.github.username).to.be.equals(user.github.username);
+		   expect(usr.github.profileUrl).to.be.equals(user.github.profileUrl);
+		   expect(usr.profile.name).to.be.equals(user.profile.name);
+		   expect(usr.profile.surname).to.be.equals(user.profile.surname);
+		   expect(usr.profile.nickname).to.be.equals(user.profile.nickname);
+		   expect(usr.profile.email).to.be.equals(user.profile.email);
 				  // expect(usr.profile.updated).to.be.equals(user.profile.updated);
 				  expect(usr.profile.visible).to.be.equals(user.profile.visible);
 
-	    		done(err);
-	    	}
-	    });
-	  });
+				  done(err);
+				}
+			});
+		});
 
 		afterEach(done => {
 			User.remove({}, err => { 
@@ -95,26 +95,26 @@ describe('users', () => {
 	});
 
 
-describe('---ERRORS---', () => {
+	describe('---ERRORS---', () => {
 		//here there are some test with empty user, because I destroyed the db 
 		//in the afterEach above.
 		
 		it('should catch 404 not found and check the error message', done => {
 			agent
 			.get('/api/users/' + 'fakeId')
-	    .set('Content-Type', 'application/json')
-	    .set('Accept', 'application/json')
-	    .expect(404)
-	    .end((err, res) => {
-	    	if (err) {
-	    		return done(err);
-	    	} else {
-	    		expect(res.body).to.be.not.null;
-	    		expect(res.body).to.be.not.undefined;
-	    		expect(res.body.message).to.be.equals('User not found');
-	    		done();
-	    	}
-	    });
-	  });
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.expect(404)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				} else {
+					expect(res.body).to.be.not.null;
+					expect(res.body).to.be.not.undefined;
+					expect(res.body.message).to.be.equals('User not found');
+					done();
+				}
+			});
+		});
 	});
 });
