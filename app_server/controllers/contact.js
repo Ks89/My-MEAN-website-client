49bac6e3fd002b7b0675219bfa1b5a14f0b75e03
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-06-23 01:19:50 +0200

    rewritten server's contact.js

--- app_server/controllers/contact.js
+++ app_server/controllers/contact.js
@@ -3,9 +3,9 @@ var nodemailer = require('nodemailer');
 var url = require('url');
 var request = require('request');
 var logger = require('../utils/logger.js');
+var Utils = require('../utils/util');
+var async = require('async');
 
-
-//TODO refactor this code!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 if(process.env.NODE_ENV === 'test') {
 	mailTransport = nodemailer.createTransport(stubTransport());
@@ -21,38 +21,6 @@ if(process.env.NODE_ENV === 'test') {
 	});
 }
 
-function send(formEmail, callback) {
-	console.log('Sending an email from ' + process.env.USER_EMAIL + ' to: ' + formEmail.email);
-	var result = 1; //never remove to prevent "return undefined"
-	if (formEmail) {
-		var message = {
-			from: process.env.USER_EMAIL, 
-			to: formEmail.email,
-			subject: formEmail.object,
-			html: formEmail.messageText, 
-			generateTextFromHtml: true
-		};
-
-		//this is an async call. You shouldn't use a "return" here.
-		//I'm using a callback function
-		mailTransport.sendMail(message, error => {
-			if (error) {
-				console.log('Error ----> returning 404');
-				console.log(error.message);
-				result = 404;
-				callback(result);
-			}
-			console.log('OK -----> returning 200');
-			result = 200;
-			callback(result);
-		});
-	} else {
-		console.log('Error ----> returning 404');
-		result = 404;
-		callback(result);
-	}
-}
-
 /* POST to send an email */
 /* /api/email */
 module.exports.sendEmailWithRecaptcha = function(req, res) {
@@ -66,43 +34,92 @@ module.exports.sendEmailWithRecaptcha = function(req, res) {
 		//here I can add also the IP, but it's not mandatory
 	};
 
-	request.post({url:'https://www.google.com/recaptcha/api/siteverify', form: data}, 
-		(err,response,body) => {
+	const RECAPTCHA_URL = 'https://www.google.com/recaptcha/api/siteverify';
 
-			if (err) {
-				console.error('Failed:', err);
-				res.status(404);
-				res.send("Error 404");
-			}
+	async.waterfall([
+    done => {
+    	if(process.env.NODE_ENV !== 'test') {
+	    	request.post({url:RECAPTCHA_URL, form: data}, (err,response,body) => {
+	    		console.log('KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK');
+	    		console.log(body);
+	    		done(err, body);
+	    	});
+	    } else {
+	    	console.log("I'm testing this method");
+	    	//TODO add something to be able to test this method.
+	    	//for example done(null, {
+			//						  "success": true,
+			//						  "challenge_ts": "2016-06-22T22:59:40Z",
+			//						  "hostname": "localhost"
+			//						 } );
+			done(null, JSON.stringify({
+					  success: true,
+					  challenge_ts: "2016-06-22T22:59:40Z",
+					  hostname: "localhost"
+					 }) );
+	    }
+    }, 
+    (body, done) => {
 
-			var result = JSON.parse( body );
-			console.log(result);
-
-			if(!result.success) {
-				res.status(404).send(result['error-codes']);
-			} else {
-				console.log("Trying to send an email");
-				if (req && req.body.emailFormData) {
-					console.log("Preparing to send an email");
-					
-					var callback = function(resultVal) {
-						console.log("callaback called with resultVal: " + resultVal);
-						if(resultVal == 200) {
-							console.log('Message sent successfully from: ' + req.body.emailFormData.email);
-							res.status(200);
-							res.send('Message sent successfully from: ' + req.body.emailFormData.email);
-						} else {
-							console.log("Error, resultVal!=200 -> " + resultVal);	
-							res.status(404);
-							res.send("Error 404");
-						}
-					};
-
-					//send the email using the callback defined above and the emailFormData obtained from the
-					//req.body. I'm accessing to emailFormData, because I build the data in contactData
-					send(req.body.emailFormData, callback);
-				}
-			}
+    	var result = JSON.parse( body );
+		console.log(result);
+		if(!result.success) {
+			Utils.sendJSONres(res, 404, result['error-codes']);
+		} else {
+			done();
 		}
-	);
+	},
+	(done) => {
+
+		console.log("Trying to send an email");
+		if (req && req.body.emailFormData) {
+			console.log("Preparing to send an email");
+			done(null, req.body.emailFormData);
+		}
+	},
+	(formEmail, done) => {
+
+		if (formEmail) {
+			console.log('Sending an email from ' + process.env.USER_EMAIL + ' to: ' + formEmail.email);
+		
+			var message = {
+				from: process.env.USER_EMAIL, 
+				to: formEmail.email,
+				subject: formEmail.object,
+				html: formEmail.messageText, 
+				generateTextFromHtml: true
+			};
+
+			//this is an async call. You shouldn't use a "return" here.
+			//I'm using a callback function
+			mailTransport.sendMail(message, error => {
+				if (error) {
+					console.log('Error ----> returning 404');
+					console.log(error.message);
+					done(null, 404, formEmail);
+				}
+				console.log('OK -----> returning 200');
+				done(null, 200, formEmail);
+			});
+		} else {
+			console.log('Error ----> returning 404');
+			done(null, 404, formEmail);
+		}
+	},
+	(resultVal, formEmail, done) => {
+
+		console.log("callaback called with resultVal: " + resultVal);
+		if(resultVal === 200) {
+			console.log('Message sent successfully from: ' + formEmail.email);
+			Utils.sendJSONres(res, 200, formEmail.email);
+		} else {
+			console.log("Error, resultVal!=200 -> " + resultVal);	
+			Utils.sendJSONres(res, 500, "Impossibile to send the email");
+		}
+
+    }], (err, user) => {
+    	if (err) {
+    		Utils.sendJSONres(res, 500, "Unknown error");
+		}
+    });
 };
