e013754fa52ffa6c5162d23600a938b12d0aa5e5
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-07-05 23:40:48 +0200

    another experimental impl of protractor tests - still not stable - there are some problems

--- test-client-e2e/projectList-test.js
+++ test-client-e2e/projectList-test.js
@@ -1,9 +1,8 @@
 'use strict';
 
-describe('my app', function() {
+describe('MyWebsite', function() {
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /home when location hash/fragment is empty', function() {
     browser.get('index.html');
     expect(browser.getLocationAbsUrl()).toMatch("/home");
   });
@@ -24,25 +23,24 @@ describe('my app', function() {
     //     return defer.promise;
     // };
 
-
-
     it('should render projects when user navigates to /projects', function() {
-
-
       expect(browser.getLocationAbsUrl()).toMatch("/projects");
 
-// browser.pause();
-      // var a = element.all(by.repeater('project in vm.projects')).get(0);
-
       element.all(by.repeater('project in vm.projects')).then(function(project) {
-       var titleElement0 = project[0].element(by.binding('project.name'));
-       var titleElement1 = project[1].element(by.binding('project.name'));
-       var titleElement2 = project[2].element(by.binding('project.name'));
+        var titleElement0 = project[0].element(by.binding('project.name'));
+        var titleElement1 = project[1].element(by.binding('project.name'));
+        var titleElement2 = project[2].element(by.binding('project.name'));
 
-       expect(titleElement0.getText()).toEqual('BYAManager');
-       expect(titleElement1.getText()).toEqual('SPF');
-       expect(titleElement2.getText()).toEqual('Superapp');
+        expect(titleElement0.getText()).toEqual('BYAManager');
+        expect(titleElement1.getText()).toEqual('SPF');
+        expect(titleElement2.getText()).toEqual('Superapp');
       });
+    });
+
+
+    it('should filter projects by name in projects page', function() {
+
+      expect(browser.getLocationAbsUrl()).toMatch("/projects");
 
       element(by.model('search.name')).sendKeys('BYA');
 
