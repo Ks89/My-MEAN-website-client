0c7bdd216796af50c84d3e06919dca04fbfb8463
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-14 00:44:45 +0200

    improved navigationbar adding login link

--- app_client/app/common/navbar/navbar.ts
+++ app_client/app/common/navbar/navbar.ts
@@ -7,4 +7,10 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
   templateUrl: 'app/common/navbar/navbar.html',
   directives: [ROUTER_DIRECTIVES]
 })
-export default class NavbarComponent {}
+export default class NavbarComponent {
+
+  //TODO FIXME replace with a real impl calling the service
+  isLoggedIn: boolean = false;
+  currentUser: any = {name : 'fake'};
+  currentPath: string = 'fakeString';
+}
