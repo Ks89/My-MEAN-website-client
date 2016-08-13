af1578b089037cf23b77144fc18d9c7d374f94ac
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-14 00:29:49 +0200

    implemented a working impl of projectList with timeline

--- app_client/app/pages/projectList/projectList.ts
+++ app_client/app/pages/projectList/projectList.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
+// import {NgIf} from '@angular/common';
 import {Observable} from "rxjs/Observable";
-
 import {Project, ProjectService} from '../../services/project-service';
 
 @Component({
@@ -12,8 +12,55 @@ import {Project, ProjectService} from '../../services/project-service';
 })
 export default class ProjectListComponent {
   projects: Observable<Project[]>;
+  pageHeader: any;
+  sidebar: any;
+  sidebarTitle: string;
+  message: string;
 
   constructor(private projectService: ProjectService) {
     this.projects = this.projectService.getProjects();
+
+    this.pageHeader = {
+      title: 'Projects',
+      strapline: ''
+    };
+
+    //init the timeline into the sidebar
+    this.sidebarTitle = "What can you do?";
+    this.sidebar = {
+      timeline: [
+      {
+        title: "Discover",
+        body: "Check my projects on GitHub.",
+        icon: "search",
+        color: "badge"
+      },
+      {
+        title: "Like",
+        body: "Star the project that you like.",
+        icon: "star",
+        color: "danger"
+      },
+      {
+        title: "Improve",
+        body: "Fork a project to enhance it.",
+        icon: "plus",
+        color: "warning"
+      },
+      {
+        title: "Collaborate",
+        body: "Create a pull request with your improvements.",
+        icon: "user",
+        color: "info"
+      },
+      {
+        title: "Share",
+        body: "Share the project with your friends or on the web.",
+        icon: "globe",
+        color: "success"
+      }
+      ]};
+
+      this.message = "Searching for projects";
   }
 }
