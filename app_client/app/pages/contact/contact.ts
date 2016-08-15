2069804fae344743aeaa4391e9647fafe8f7a773
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-15 23:38:56 +0200

    first impl of the contact page

--- app_client/app/pages/contact/contact.ts
+++ app_client/app/pages/contact/contact.ts
@@ -1,21 +1,47 @@
 import {Component} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import PageHeaderComponent from '../../common/pageHeader/pageHeader';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {
+    FormControl,
+    FormGroup,
+    FormBuilder,
+    Validators,
+    REACTIVE_FORM_DIRECTIVES
+} from '@angular/forms';
 
 @Component({
   selector: 'contact-page',
   providers: [],
-  directives: [PageHeaderComponent],
+  directives: [ROUTER_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, PageHeaderComponent],
   // styleUrls: ['app/pages/contact/contact.css'],
   templateUrl: 'app/pages/contact/contact.html'
 })
 export default class ContactComponent {
   pageHeader: any;
 
+  publicKey: string = "6Lf0jxQTAAAAAIDxhvAqGseKy_KV2_4iViVeQWYi"; //not secret, no problem ;)
+
+  formModel: FormGroup;
+
   constructor() {
     this.pageHeader = {
       title: 'Contact',
       strapline: ''
     };
+
+    const fb = new FormBuilder();
+    this.formModel = fb.group({
+      'email': [null, Validators.minLength(3)],
+      'subject': [null, Validators.minLength(3)],
+      'message': [null, Validators.minLength(3)],
+    })
   }
-}
+
+  onSend() {
+    if (this.formModel.valid) {
+        console.log("Sending email...");
+        //this.authService.loginEvent.emit(this.formModel.value);
+      }
+    }
+  }
