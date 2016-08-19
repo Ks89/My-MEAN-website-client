11d6dadf89f94ac1d88421dfc40b2b9b5da91823
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-19 20:54:44 +0200

    impl forgot page

--- app_client/app/pages/forgot/forgot.ts
+++ app_client/app/pages/forgot/forgot.ts
@@ -1,5 +1,11 @@
 import {Component} from '@angular/core';
 import {Observable} from "rxjs/Observable";
+import {
+    FormControl,
+    FormGroup,
+    FormBuilder,
+    Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'forgot-page',
@@ -9,11 +15,24 @@ import {Observable} from "rxjs/Observable";
 })
 export default class ForgotComponent {
   pageHeader: any;
+  formModel: FormGroup;
 
   constructor() {
     this.pageHeader = {
       title: 'Forgot',
       strapline: ''
     };
+
+    const fb = new FormBuilder();
+    this.formModel = fb.group({
+      'email': [null, Validators.minLength(3)]
+    })
+  }
+
+  onForgot() {
+    if (this.formModel.valid) {
+      console.log("Calling forgot...");
+      //this.authService.forgotEvent.emit(this.formModel.value);
+    }
   }
 }
