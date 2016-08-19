9e5f2680494121b23761a2d1d7021a8f48c2fb60
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-19 20:54:36 +0200

    impl register page

--- app_client/app/pages/register/register.ts
+++ app_client/app/pages/register/register.ts
@@ -1,5 +1,11 @@
 import {Component} from '@angular/core';
 import {Observable} from "rxjs/Observable";
+import {
+    FormControl,
+    FormGroup,
+    FormBuilder,
+    Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'register-page',
@@ -9,11 +15,26 @@ import {Observable} from "rxjs/Observable";
 })
 export default class RegisterComponent {
   pageHeader: any;
+  formModel: FormGroup;
 
   constructor() {
     this.pageHeader = {
       title: 'Create a new accout',
       strapline: ''
     };
+
+    const fb = new FormBuilder();
+    this.formModel = fb.group({
+      'name': [null, Validators.minLength(3)],
+      'email': [null, Validators.minLength(3)],
+      'password': [null, Validators.minLength(3)]
+    })
+  }
+
+  onRegister() {
+    if (this.formModel.valid) {
+      console.log("Calling register...");
+      //this.authService.registerEvent.emit(this.formModel.value);
+    }
   }
 }
