a5eca19b5599164a3687ed3dc30ae40d036dfa21
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-27 01:36:25 +0200

    first working impl of webpack + angular2. Still without bootstrap css. I included only bootstrap js.

--- app_client/app/vendor.ts
+++ app_client/app/vendor.ts
@@ -1,28 +1,30 @@
 import 'zone.js/dist/zone';
-import 'reflect-metadata/Reflect.js';
+import 'reflect-metadata';
 
 // Angular modules
+import '@angular/core';
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/forms';
+import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/http';
 import '@angular/router';
-import '@angular/common';
-import '@angular/compiler';
-import '@angular/core';
-import '@angular/forms';
-import '@angular/platform-browser';
+
+// RxJS
+import 'rxjs';
 
 // import 'angular2-recaptcha';
 // import 'angular2-image-popup/app';
 import 'h5webstorage';
 import '@ng-bootstrap/ng-bootstrap';
 // import 'angular2-component-outlet-modified';
-// RxJS
-import 'rxjs';
+
 
 // Other vendor libraries
 import 'jquery';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'tether/dist/js/tether.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'angular2-image-popup/directives/angular2-image-popup/css/style.css';
 // import 'angular2-image-popup/app/assets/css/main.css';
