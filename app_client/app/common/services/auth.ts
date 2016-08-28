3311bfe872239b91ef038fd988d03e0f33c5b10d
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-28 22:22:05 +0200

    implemented other auth methods

--- app_client/app/common/services/auth.ts
+++ app_client/app/common/services/auth.ts
@@ -16,51 +16,94 @@ export class AuthService {
     private localStorage: LocalStorage,
     private sessionStorage: SessionStorage) {}
 
-    //----------------------------------------------------------
-    //------------------ local authentication ------------------
-    //----------------------------------------------------------
+  //----------------------------------------------------------
+  //------------------ local authentication ------------------
+  //----------------------------------------------------------
 
-    login(user: any): Observable<any> {
-      return this.http.post('/api/login', user, this.options)
-      .map(response => {
-        this.sessionStorage.setItem('auth', response.json().token);
-        response.json();
-      }); //on error removeToken('auth');
-    }
+  login(user: any): Observable<any> {
+    return this.http.post('/api/login', user, this.options)
+    .map(response => {
+      this.sessionStorage.setItem('auth', response.json().token);
+      response.json();
+    }); //on error removeToken('auth');
+  }
 
-    register(user: any): Observable<any> {
-      return this.http.post('/api/register', user, this.options)
-      .map(response => {
-        // saveToken('auth', data.token);
-        this.sessionStorage.setItem('auth', response.json().token);
-        response.json();
-      }); //on error removeToken('auth');
-    }
+  register(user: any): Observable<any> {
+    return this.http.post('/api/register', user, this.options)
+    .map(response => {
+      // saveToken('auth', data.token);
+      this.sessionStorage.setItem('auth', response.json().token);
+      response.json();
+    }); //on error removeToken('auth');
+  }
 
-    reset(emailToken: any, newPassword: any): Observable<any> {
-      let data = {
-        newPassword : newPassword,
-        emailToken : emailToken
-      };
-      return this.http.post('/api/resetNewPassword', data, this.options)
-      .map(response => {
-        // saveToken('auth', data.token);
-        this.sessionStorage.setItem('auth', response.json().token);
-        response.json();
-      });
-    }
+  reset(emailToken: any, newPassword: any): Observable<any> {
+    let data = {
+      newPassword : newPassword,
+      emailToken : emailToken
+    };
+    return this.http.post('/api/resetNewPassword', data, this.options)
+    .map(response => {
+      // saveToken('auth', data.token);
+      this.sessionStorage.setItem('auth', response.json().token);
+      response.json();
+    });
+  }
 
-    forgot(email: any): Observable<any> {
-      return this.http.post('/api/reset', { email : email}, this.options)
-      .map(response => response.json());
-    }
+  forgot(email: any): Observable<any> {
+    return this.http.post('/api/reset', { email : email}, this.options)
+    .map(response => response.json());
+  }
 
-    activate(emailToken: string, userName: string): Observable<any> {
-      let data = {
-        emailToken : emailToken,
-        userName : userName
-      };
-      return this.http.post('/api/activateAccount', data, this.options)
-      .map(response => response.json());
-    }
+  activate(emailToken: string, userName: string): Observable<any> {
+    let data = {
+      emailToken : emailToken,
+      userName : userName
+    };
+    return this.http.post('/api/activateAccount', data, this.options)
+    .map(response => response.json());
+  }
+
+  unlink(serviceName: string): Observable<any> {
+    console.log("Called unlink " + serviceName);
+    return this.http.get(`/api/unlink/${serviceName}`)
+    .map(response => response.json());
   };
+
+  //-----------------------------
+  //--- 3dauth authentication ---
+  //-----------------------------
+
+
+
+
+  //---------------------------------------
+  //--- local and 3dauth authentication ---
+  //---------------------------------------
+  //function to call the /users/:id REST API
+  getUserById(id: string): Observable<any> {
+    return this.http.get(`/api/users/${id}`)
+    .map(response => response.json());
+  };
+
+  logout(): Observable<any> {
+    console.log("Called authentication logout");
+    this.sessionStorage.removeItem('auth');
+
+    //call REST service to remove session data from redis
+    return this.http.get('/api/logout')
+    .map(response => response.json());
+  };
+
+  getTokenRedis(): Observable<any> {
+    return this.http.get('/api/sessionToken')
+    .map(response => response.json());
+  };
+
+  decodeJwtToken(jwtToken: string): Observable<any> {
+    //TODO add an if(jwtToken) or something like that
+    return this.http.get(`/api/decodeToken/${jwtToken}`)
+    .map(response => response.json());
+  };
+
+};
