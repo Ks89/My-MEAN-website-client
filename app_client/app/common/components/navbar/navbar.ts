b96ce8524af80cea569f7151721208d6058fa09f
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-08-31 02:24:30 +0200

    implemented logout into the navbar component

--- app_client/app/common/components/navbar/navbar.ts
+++ app_client/app/common/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'navigation',
@@ -10,22 +11,38 @@ export default class NavbarComponent {
 
   //TODO FIXME replace with a real impl calling the service
   isLoggedIn: boolean = false;
-  currentUser: any = {name : 'fake'};
+  currentUser: any = null;
   currentPath: string = 'fakeString';
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
 
     this.authService.loginEvent
-        .subscribe(res =>
-        {
-          console.log("NAVIGATION: ");
-          console.log(res);
+    .subscribe(res => {
+        console.log("NAVIGATION: ");
+        console.log(res);
+        this.currentUser = {name: res.local.name};
+        this.isLoggedIn = true;
+      },err =>  console.log("Can't get logged user"),
+      () => console.log('DONE')
+    );
+  }
 
-          this.currentUser = {name: res.local.name};
-          this.isLoggedIn = true;
-
-        },err =>  console.log("Can't get logged user"),
-        () => console.log('DONE')
-      );
+  logout() {
+    this.authService.logout()
+    .subscribe(
+      result => {
+        console.log('Logout result: ');
+        console.log(result.message);
+        this.isLoggedIn = false;
+        this.currentUser = null;
+      }, err => {
+        console.log('Impossibile to logout: ' + err);
+        this.isLoggedIn = false; //FIXME, Choose the value, I don't know, but I suppose "false"
+        this.currentUser = null;
+      }, () => {
+        console.log("logout finished");
+        this.router.navigate(['/']);
+      }
+    );
   }
 }
