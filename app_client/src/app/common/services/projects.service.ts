11808ba1b75e28d4994f8fa7091ac78ebe48a519
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-10-12 00:12:16 +0200

    i forgot to add an import :(

--- app_client/src/app/common/services/projects.service.ts
+++ app_client/src/app/common/services/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 export class ProjectHomeView {
   constructor(
