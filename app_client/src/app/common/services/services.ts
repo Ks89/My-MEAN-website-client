07f5fa1cc93a87649ff15bb294b36f9fbe0e9ede
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-10-13 01:40:46 +0200

    fixed a stupid bug. I forgot to export the auth-guard's service

--- app_client/src/app/common/services/services.ts
+++ app_client/src/app/common/services/services.ts
@@ -2,7 +2,8 @@ import { ProjectService } from './projects.service';
 import { ProfileService } from './profile.service';
 import { ContactService } from './contact.service';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 export const SERVICES = [
-  ProjectService, ProfileService, ContactService, AuthService
+  ProjectService, ProfileService, ContactService, AuthService, AuthGuard
 ];
