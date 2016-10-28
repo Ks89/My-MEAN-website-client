6a1b76118a6dd92861ae99aaa992cb3a8f9ba3ea
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-10-29 01:07:29 +0200

    improved travis ci config and fixed issue #29

--- app_server/controllers/authentication/3dparty/3dpartyconfig.js
+++ app_server/controllers/authentication/3dparty/3dpartyconfig.js
@@ -2,33 +2,33 @@ module.exports = {
   facebook: {
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3100/api/auth/facebook/callback",
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
     profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
     passReqToCallback: true
   },
   twitter: {
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/api/auth/twitter/callback",
+    callbackURL: process.env.TWITTER_CALLBACK_URL,
     userProfileURL: "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true",
     passReqToCallback : true
   },
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3100/api/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback: true
   },
   github: {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3100/api/auth/github/callback",
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
     passReqToCallback: true
   },
   linkedin: {
     clientID: process.env.LINKEDIN_CLIENT_ID,
     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    callbackURL: "http://localhost:3100/api/auth/linkedin/callback",
+    callbackURL: process.env.LINKEDIN_CALLBACK_URL,
     profileFields: ['id', 'first-name', 'last-name', 'email-address'],
     passReqToCallback: true,
     state: true
