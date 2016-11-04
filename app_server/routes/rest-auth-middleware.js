72a5f809935d7eac0805a2a9ae703dfbf577f907
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-04 02:02:51 +0100

    fixed a possible bu into the authentication middlewareg. Now I catch every exception to return 403

--- app_server/routes/rest-auth-middleware.js
+++ app_server/routes/rest-auth-middleware.js
@@ -3,46 +3,51 @@ var logger = require('../utils/logger');
 var Utils = require('../utils/util');
 
 module.exports.restAuthenticationMiddleware = function(req, res, next) {
-	logger.debug("route middleware to authenticate and check token: " + req.session.authToken);
-	if (req.session.authToken) {
-		// var cookie = JSON.parse(req.cookies.userCookie);
-		// var token = cookie.token;
-		var authToken = JSON.parse(req.session.authToken);
-		var token = authToken.token;
-		logger.debug("token: " + token);
-		if(!token) {
-			Utils.sendJSONres(res, 404, "Token not found");
-			return;
-		}
-
-		jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-			if(err) {
-				logger.error("jwt.verify error");
-				Utils.sendJSONres(res, 401, "Jwt not valid or corrupted");
+	try {
+		logger.debug("route middleware to authenticate and check token: " + req.session.authToken);
+		if (req.session.authToken) {
+			// var cookie = JSON.parse(req.cookies.userCookie);
+			// var token = cookie.token;
+			var authToken = JSON.parse(req.session.authToken);
+			var token = authToken.token;
+			logger.debug("token: " + token);
+			if(!token) {
+				Utils.sendJSONres(res, 404, "Token not found");
 				return;
 			}
 
-			if(decoded) {
-				console.log("decoded valid");
-				try {
-					if(Utils.isJwtValidDate(decoded)) {
-						logger.debug("systemDate valid");
-						next();
-					} else {
-						logger.error('No data valid');
-						Utils.sendJSONres(res, 404, "Data is not valid");
-					}
-				} catch (e) {
-					logger.error(e);
-					Utils.sendJSONres(res, 500, "Impossible to check if jwt is valid");
+			jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+				if(err) {
+					logger.error("jwt.verify error");
+					Utils.sendJSONres(res, 401, "Jwt not valid or corrupted");
+					return;
 				}
-			} else {
-				Utils.sendJSONres(res, 500, "Impossible to check if jwt is valid - decoded is not valid");
-			}
-		});
 
-	} else {
-		logger.error('No token');
+				if(decoded) {
+					console.log("decoded valid");
+					try {
+						if(Utils.isJwtValidDate(decoded)) {
+							logger.debug("systemDate valid");
+							next();
+						} else {
+							logger.error('No data valid');
+							Utils.sendJSONres(res, 404, "Data is not valid");
+						}
+					} catch (e) {
+						logger.error(e);
+						Utils.sendJSONres(res, 500, "Impossible to check if jwt is valid");
+					}
+				} else {
+					Utils.sendJSONres(res, 500, "Impossible to check if jwt is valid - decoded is not valid");
+				}
+			});
+
+		} else {
+			logger.error('restAuthenticationMiddleware - req.session.authToken not valid');
+			Utils.sendJSONres(res, 403, 'No token provided.');
+		}
+	} catch(e) {
+		logger.error('restAuthenticationMiddleware - exception catched');
 		Utils.sendJSONres(res, 403, 'No token provided.');
 	}
 };
