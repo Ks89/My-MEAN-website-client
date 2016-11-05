4908614478958fe9df0a39e903c644cd6c7c0235
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-05 18:26:59 +0100

    I forgot to add testing controller #24

--- app_server/controllers/testing.js
+++ app_server/controllers/testing.js
@@ -36,12 +36,21 @@ var setJsonWithWrongFormatTokenSession = function(req, res) {
 };
 
 var setJsonWithExpiredDateSession = function(req, res) {
+  var expiry = new Date();
+	expiry.setTime(expiry.getTime() - 600000); //expired 10 minutes ago (10*60*1000)
+  console.warn("+++++++++++++++++++++++++++++++++");
   req.session.authToken = JSON.stringify({
-    _id: 'fakeid',
-    token: 'wrong.format', // only one dot :)
-    user: null,
-    exp: (new Date()).getTime()-50000 // obviously wrong
+    _id: '581d01b3706b0a34de2c0b03',
+    user: {
+      _id: "581d01b3706b0a34de2c0b03",
+			local: {
+				email: 'email',
+				name: 'name'
+			}
+		},
+    exp: parseFloat(expiry.getTime()) // obviously wrong
   });
+  console.warn(req.session.authToken);
   console.log('setJsonWithExpiredDateSession forced -> req.session.authToken = a JSON');
   Utils.sendJSONres(res, 200, {message: "Session modified correctly!"});
 };
