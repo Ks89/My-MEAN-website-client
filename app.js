b7f50bee7cc40b1c372e544e046db0a9b5adcb3a
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-07 23:06:10 +0100

    upgrade to helmet 3.1.0 - issue #42

--- app.js
+++ app.js
@@ -44,18 +44,26 @@ var redis   = require("redis"); //it's really useful?
 var RedisStore = require('connect-redis')(session);
 var client  = redis.createClient(); //it's really useful?
 
-//[http params pollution] security package to prevent http params pollution
+// --------------------------------------------------------------------------
+// ----------------------------security packages-----------------------------
+// --------------------------------------------------------------------------
+// All security features are prefixed with `--SEC--`
+// --SEC-- - github analog-nico/hpp [NOT helmet]
+//    [http params pollution] security package to prevent http params pollution
 var hpp = require('hpp');
-
-//[large payload attacks] Make sure this application is not vulnerable to large payload attacks
-var contentLength = require('express-content-length-validator');
-var MAX_CONTENT_LENGTH_ACCEPTED = 9999;
-
-//[CSRF]
+// --SEC-- - [CSRF] github.com/expressjs/csurf [NOT helmet]
 var csrf = require('csurf');
-
-var fs = require('fs');
+// --SEC-- - authentication local/3dparty (OAuth)
 var passport = require('passport');
+// --SEC-- - github ericmdantas/express-content-length-validator [NOT helmet]
+//    large payload attacks - Make sure this application is
+//    not vulnerable to large payload attacks
+var contentLength = require('express-content-length-validator');
+const MAX_CONTENT_LENGTH_ACCEPTED = 9999; // constants used with `contentLength`
+// --SEC-- - Helmet
+var helmet = require('helmet');
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
 
 console.log("Initializing mongodb");
 //require for mongo
@@ -65,50 +73,52 @@ require('./app_server/controllers/authentication/passport')(passport);
 console.log("Initializing expressjs");
 var app = express();
 
-//[hemlet] enable hemlet
-//this automatically add 7 of 10 security features
+console.log("Initializing helmet");
+// --SEC-- - [helmet] enable helmet
+// this automatically add 9 of 11 security features
 /*
-  -contentSecurityPolicy for setting Content Security Policy
   -dnsPrefetchControl controls browser DNS prefetching
   -frameguard to prevent clickjacking
   -hidePoweredBy to remove the X-Powered-By header
+  -hpkp for HTTP Public Key Pinning
   -hsts for HTTP Strict Transport Security
   -ieNoOpen sets X-Download-Options for IE8+
-  -noCache to disable client-side caching
   -noSniff to keep clients from sniffing the MIME type
   -xssFilter adds some small XSS protections
 */
-//The other features NOT included by default are:
+// The other features NOT included by default are:
 /*
-  -hpkp for HTTP Public Key Pinning
   -contentSecurityPolicy for setting Content Security Policy
   -noCache to disable client-side caching => I don't want this for better performances
+  -referrerPolicy to hide the Referer header
 */
-console.log("Initializing hemlet");
-var helmet = require('helmet');
 app.use(helmet());
 
-// [Public Key Pinning: hpkp] HTTPS certificates can be forged, allowing man-in-the middle attacks.
-//                      HTTP Public Key Pinning aims to help that.
-var ninetyDaysInMilliseconds = 7776000000;
+// --SEC-- - noCache to disable client-side caching [helmet]
+// I don't want this for better performances (leave commented :))
+// app.use(helmet.noCache())
+
+// --SEC-- - referrer-policy to hide the Referer header [helmet]
+app.use(helmet.referrerPolicy({ policy: 'no-referrer' }))
+
+// --SEC-- - Public Key Pinning (hpkp): HTTPS certificates can be forged,
+//    allowing man-in-the middle attacks.
+//    HTTP Public Key Pinning aims to help that. [helmet]
+const ninetyDaysInSeconds = 7776000;
 app.use(helmet.hpkp({
-  maxAge: ninetyDaysInMilliseconds,
+  maxAge: ninetyDaysInSeconds,
   sha256s: ['AbCdEf123=', 'ZyXwVu456='],
   includeSubdomains: true,         // optional
-  reportUri: 'http://example.com',  // optional
+  reportUri: 'https://example.com/hpkp-report', // optional
   reportOnly: false,               // optional
-
   // Set the header based on a condition.
-  // This is optional.
-  setIf: function (req, res) {
-    return req.secure;
-  }
+  setIf: (req, res)  => req.secure //optional ()
 }));
 
-// [CSP - Content Security Policy] Trying to prevent: Injecting anything unintended into our page.
-//                   That could cause XSS vulnerabilities, unintended tracking, malicious frames, and more.
+// --SEC-- - Content Security Policy (CSP): Trying to prevent Injecting anything
+//    unintended into our page. That could cause XSS vulnerabilities,
+//    unintended tracking, malicious frames, and more. [helmet]
 app.use(helmet.contentSecurityPolicy({
-  // Specify directives as normal.
   directives: {
     defaultSrc: ["'self'", 'localhost:3000', 'localhost:3001', 'www.google.com', 'www.youtube.com'],
     scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'maxcdn.bootstrapcdn.com',
@@ -124,36 +134,33 @@ app.use(helmet.contentSecurityPolicy({
     frameSrc : ["'self'", 'www.google.com', 'www.youtube.com'], //frame-src is deprecated
     childSrc : ["'self'", 'www.google.com', 'www.youtube.com'],
     connectSrc: [
-        "'self'",
-        "cdnjs.cloudflare.com",
-        "ajax.googleapis.com",
-        "ws://localhost:3000",
-        "ws://localhost:3001"
+        "'self'", "cdnjs.cloudflare.com", "ajax.googleapis.com",
+        "ws://localhost:3000", "ws://localhost:3001", "ws://localhost:3100",
+        "ws://localhost:3300"
     ],
     reportUri: '/report-violation',
-    objectSrc: [] // An empty array allows nothing through
+    objectSrc: ["'none'"]
   },
-
   // Set to true if you only want browsers to report errors, not block them
   reportOnly: false,
-
   // Set to true if you want to blindly set all headers: Content-Security-Policy,
   // X-WebKit-CSP, and X-Content-Security-Policy.
   setAllHeaders: false,
-
   // Set to true if you want to disable CSP on Android where it can be buggy.
   disableAndroid: false,
-
   // Set to false if you want to completely disable any user-agent sniffing.
   // This may make the headers less compatible but it will be much faster.
   // This defaults to 'true'.
   browserSniff: true
 }));
 
-//[large payload attacks] this line enables the middleware for all routes
-app.use(contentLength.validateMax({max: MAX_CONTENT_LENGTH_ACCEPTED, status: 400, message: "stop it!"})); // max size accepted for the content-length
+// --SEC-- - large payload attacks:
+//   this line enables the middleware for all routes [NOT helmet]
+app.use(contentLength.validateMax({max: MAX_CONTENT_LENGTH_ACCEPTED,
+  status: 400, message: "stop it!"})); // max size accepted for the content-length
 
-console.log("Initializing morgan");
+
+console.log("Initializing morgan (logger)");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -167,10 +174,12 @@ console.log("Initializing bodyparser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//[http params pollution] activate http parameters pollution
-//use this ALWAYS AFTER app.use(bodyParser.urlencoded())
+console.log("Initializing hpp");
+// --SEC-- - http params pollution: activate http parameters pollution
+// use this ALWAYS AFTER app.use(bodyParser.urlencoded()) [NOT helmet]
 app.use(hpp());
 
+console.log("Initializing Express session");
 // Express Session
 app.use(session({
     secret: 'keyboard cat',
@@ -192,7 +201,7 @@ app.use(passport.session());
 // compress all requests using gzip
 app.use(compression());
 
-console.log("Initializing REST apis");
+console.log("Initializing REST apis and CSRF");
 
 // --------------------------------------- ROUTES ---------------------------------------
 // dedicated routes for angular logging with stacktracejs
@@ -200,7 +209,7 @@ console.log("Initializing REST apis");
 var loggerApi = require('./app_server/routes/log-api')(express);
 app.use('/api/log', loggerApi);
 
-// enable middleware CSRF by csurf package
+// enable middleware CSRF by csurf package [NOT helmet]
 // before app.use('/api', routesApi); to protect their,
 // but after session and/or cookie initialization
 app.use(csrf());
@@ -214,7 +223,6 @@ app.use(function (req, res, next) {
 var routesApi = require('./app_server/routes/index')(express);
 app.use('/api', routesApi);
 // --------------------------------------------------------------------------------------
-
 
 console.log("Initializing static path for index.html");
 
