f0fa3ec94303156885652b657445f83383b2b4aa
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-08 22:22:59 +0100

    refactoring

--- travisci/before_script.sh
+++ travisci/before_script.sh
@@ -1,9 +1,11 @@
+npm install -g typings
 npm install -g karma-cli
 npm install -g webpack
 npm install -g typescript
-npm install -g typings
 npm install -g remap-istanbul
 npm install -g webdriver-manager
 npm install -g protractor
 npm install -g codeclimate-test-reporter
 npm install -g istanbul
+
+npm install
