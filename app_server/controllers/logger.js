fba1d60ce3c301dbfd2ed520faf8e60985a0933d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-08 01:39:39 +0100

    apidocjs for logger.js #50

--- app_server/controllers/logger.js
+++ app_server/controllers/logger.js
@@ -1,20 +1,107 @@
 var logger = require('../utils/logger.js');
 var Utils = require('../utils/util.js');
 
-/* POST to log a debug message */
-/* /api/error */
+/**
+* @api {post} /api/error Log an error message on server
+* @apiVersion 0.0.1
+* @apiName PostError
+* @apiGroup Logger
+* @apiPermission none
+*
+* @apiDescription Send an error message to the server.
+*
+* @apiParam {String} message Message to log.
+*
+* @apiHeaderExample {json} Header-Example:
+*     {
+*       "Content-Type": "application/json",
+*				"Accept": "application/json"
+*     }
+*
+* @apiSuccess {String} info Constant message: "error logged on server".
+* @apiSuccess {Object} body Object that contains the response.
+* @apiSuccess {String} body.message The same message received as parameter.
+*
+* @apiSuccessExample {json} Success-Response:
+*   HTTP/1.1 200 OK
+*   {
+*     "info": "error logged on server",
+*     "body":
+*     {
+*       "message": "an error message to log"
+*     }
+*   }
+*/
 module.exports.error = (req, res) => {
 	log(req, res, "error");
 };
 
-/* POST to log a debug message */
-/* /api/debug */
+/**
+* @api {post} /api/debug Log a debug message on server
+* @apiVersion 0.0.1
+* @apiName PostDebug
+* @apiGroup Logger
+* @apiPermission none
+*
+* @apiDescription Send a debug message to the server.
+*
+* @apiParam {String} message Message to log.
+*
+* @apiHeaderExample {json} Header-Example:
+*     {
+*       "Content-Type": "application/json",
+*				"Accept": "application/json"
+*     }
+*
+* @apiSuccess {String} info Constant message: "debug logged on server".
+* @apiSuccess {Object} body Object that contains the response.
+* @apiSuccess {String} body.message The same message received as parameter.
+*
+* @apiSuccessExample {json} Success-Response:
+*   HTTP/1.1 200 OK
+*   {
+*     "info": "debug logged on server",
+*     "body":
+*     {
+*       "message": "a debug message to log"
+*     }
+*   }
+*/
 module.exports.debug = (req, res) => {
 	log(req, res, "debug");
 };
 
-/* POST to log an error message */
-/* /api/exception */
+/**
+* @api {post} /api/exception Log an exception message on server
+* @apiVersion 0.0.1
+* @apiName PostException
+* @apiGroup Logger
+* @apiPermission none
+*
+* @apiDescription Send an exception message to the server.
+*
+* @apiParam {String} message Message to log.
+*
+* @apiHeaderExample {json} Header-Example:
+*     {
+*       "Content-Type": "application/json",
+*				"Accept": "application/json"
+*     }
+*
+* @apiSuccess {String} info Constant message: "exception logged on server".
+* @apiSuccess {Object} body Object that contains the response.
+* @apiSuccess {String} body.message The same message received as parameter.
+*
+* @apiSuccessExample {json} Success-Response:
+*   HTTP/1.1 200 OK
+*   {
+*     "info": "Exception logged on server",
+*     "body":
+*     {
+*       "message": "an exception message to log"
+*     }
+*   }
+*/
 module.exports.exception = (req, res) => {
 	console.error("Called log-exception = ");
 	console.error(req.body);
