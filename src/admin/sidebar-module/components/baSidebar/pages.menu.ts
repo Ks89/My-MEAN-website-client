506ebd125bb4edf89ca458dfd935a9029ea2a71a
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-13 23:56:51 +0100

    admin menu improved #2

--- src/admin/sidebar-module/components/baSidebar/pages.menu.ts
+++ src/admin/sidebar-module/components/baSidebar/pages.menu.ts
@@ -15,10 +15,10 @@ export const PAGES_MENU = [
         }
       },
       {
-        path: 'editors',
+        path: 'users',
         data: {
           menu: {
-            title: 'Editors',
+            title: 'Users',
             icon: 'ion-edit',
             selected: false,
             expanded: false,
@@ -27,20 +27,30 @@ export const PAGES_MENU = [
         },
         children: [
           {
-            path: 'ckeditor',
+            path: 'allUser',
             data: {
               menu: {
-                title: 'CKEditor',
+                title: 'All',
+                url: '/allUser'
+              }
+            }
+          },
+          {
+            path: 'singleUser',
+            data: {
+              menu: {
+                title: 'Single',
+                url: '/singleUser'
               }
             }
           }
         ]
       },
       {
-        path: 'components',
+        path: 'newsletter',
         data: {
           menu: {
-            title: 'Components',
+            title: 'Newsletter',
             icon: 'ion-gear-a',
             selected: false,
             expanded: false,
@@ -49,270 +59,97 @@ export const PAGES_MENU = [
         },
         children: [
           {
-            path: 'treeview',
+            path: 'newsletterSearch',
             data: {
               menu: {
-                title: 'Tree View',
+                title: 'Search',
+                url: '/newsletterSearch'
+              }
+            }
+          },
+          {
+            path: 'newsletterCreate',
+            data: {
+              menu: {
+                title: 'Create',
+                url: '/newsletterCreate'
               }
             }
           }
         ]
       },
-      {
-        path: 'charts',
-        data: {
-          menu: {
-            title: 'Charts',
-            icon: 'ion-stats-bars',
-            selected: false,
-            expanded: false,
-            order: 200,
-          }
-        },
-        children: [
-          {
-            path: 'chartist-js',
-            data: {
-              menu: {
-                title: 'Chartist.Js',
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: 'ui',
-        data: {
-          menu: {
-            title: 'UI Features',
-            icon: 'ion-android-laptop',
-            selected: false,
-            expanded: false,
-            order: 300,
-          }
-        },
-        children: [
-          {
-            path: 'typography',
-            data: {
-              menu: {
-                title: 'Typography',
-              }
-            }
-          },
-          {
-            path: 'buttons',
-            data: {
-              menu: {
-                title: 'Buttons',
-              }
-            }
-          },
-          {
-            path: 'icons',
-            data: {
-              menu: {
-                title: 'Icons',
-              }
-            }
-          },
-          {
-            path: 'grid',
-            data: {
-              menu: {
-                title: 'Grid',
-              }
-            }
-          },
-        ]
-      },
-      {
-        path: 'forms',
-        data: {
-          menu: {
-            title: 'Form Elements',
-            icon: 'ion-compose',
-            selected: false,
-            expanded: false,
-            order: 400,
-          }
-        },
-        children: [
-          {
-            path: 'inputs',
-            data: {
-              menu: {
-                title: 'Form Inputs',
-              }
-            }
-          },
-          {
-            path: 'layouts',
-            data: {
-              menu: {
-                title: 'Form Layouts',
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: 'tables',
-        data: {
-          menu: {
-            title: 'Tables',
-            icon: 'ion-grid',
-            selected: false,
-            expanded: false,
-            order: 500,
-          }
-        },
-        children: [
-          {
-            path: 'basictables',
-            data: {
-              menu: {
-                title: 'Basic Tables',
-              }
-            }
-          },
-          {
-            path: 'smarttables',
-            data: {
-              menu: {
-                title: 'Smart Tables',
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: 'maps',
-        data: {
-          menu: {
-            title: 'Maps',
-            icon: 'ion-ios-location-outline',
-            selected: false,
-            expanded: false,
-            order: 600,
-          }
-        },
-        children: [
-          {
-            path: 'googlemaps',
-            data: {
-              menu: {
-                title: 'Google Maps',
-              }
-            }
-          },
-          {
-            path: 'leafletmaps',
-            data: {
-              menu: {
-                title: 'Leaflet Maps',
-              }
-            }
-          },
-          {
-            path: 'bubblemaps',
-            data: {
-              menu: {
-                title: 'Bubble Maps',
-              }
-            }
-          },
-          {
-            path: 'linemaps',
-            data: {
-              menu: {
-                title: 'Line Maps',
-              }
-            }
-          }
-        ]
-      },
+      // {
+      //   path: '',
+      //   data: {
+      //     menu: {
+      //       title: 'Pages',
+      //       icon: 'ion-document',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 650,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: '',
+      //       data: {
+      //         menu: {
+      //           title: 'Login',
+      //           url: '/login'
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
+      // {
+      //   path: '',
+      //   data: {
+      //     menu: {
+      //       title: 'Menu Level 1',
+      //       icon: 'ion-ios-more',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 700,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: '',
+      //       data: {
+      //         menu: {
+      //           title: 'Menu Level 1.1',
+      //           url: '#'
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: '',
+      //       data: {
+      //         menu: {
+      //           title: 'Menu Level 1.2',
+      //           url: '#'
+      //         }
+      //       },
+      //       children: [
+      //         {
+      //           path: '',
+      //           data: {
+      //             menu: {
+      //               title: 'Menu Level 1.2.1',
+      //               url: '#'
+      //             }
+      //           }
+      //         }
+      //       ]
+      //     }
+      //   ]
+      // },
       {
         path: '',
         data: {
           menu: {
-            title: 'Pages',
-            icon: 'ion-document',
-            selected: false,
-            expanded: false,
-            order: 650,
-          }
-        },
-        children: [
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'Login',
-                url: '#/login'
-              }
-            }
-          },
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'Register',
-                url: '#/register'
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'Menu Level 1',
-            icon: 'ion-ios-more',
-            selected: false,
-            expanded: false,
-            order: 700,
-          }
-        },
-        children: [
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'Menu Level 1.1',
-                url: '#'
-              }
-            }
-          },
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'Menu Level 1.2',
-                url: '#'
-              }
-            },
-            children: [
-              {
-                path: '',
-                data: {
-                  menu: {
-                    title: 'Menu Level 1.2.1',
-                    url: '#'
-                  }
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'External Link',
-            url: 'http://akveo.com',
+            title: 'Github',
+            url: 'http://github.com/ks89',
             icon: 'ion-android-exit',
             order: 800,
             target: '_blank'
