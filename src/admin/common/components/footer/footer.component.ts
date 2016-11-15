1c52112cdeb6b48635f480a33fb00e8a9f8ec15e
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-15 22:37:17 +0100

    footer removed #2

--- src/admin/common/components/footer/footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mmw-admin-footer',
-  styleUrls: ['footer.css'],
-  templateUrl: 'footer.html'
-})
-export default class FooterAdminComponent {}
