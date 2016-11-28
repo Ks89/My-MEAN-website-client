63f469a578ae739244645b9dfeffeaa8ee87164a
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-11-28 23:20:21 +0100

    finally restored noParse in webpack.common #19

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -84,8 +84,8 @@ module.exports = {
       },
       // Bootstrap 4
       { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' }
-    ]
-    // noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
+    ],
+    noParse: [/node_modules\/angular2\/bundles\//]
   },
   plugins: [
     new ManifestPlugin(), // TODO check if I can remove this
