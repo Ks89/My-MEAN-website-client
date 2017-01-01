3d35f0e6e5ba7ac452d22daddbd5e8b7f531e383
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-01 23:05:36 +0100

    npm scripts from ./node_modules/.bin insted of a global location. Also, I added coveralls #41

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,6 +1,13 @@
-# add command here if you want
+#!/usr/bin/env bash
+
+# run debug builf
 npm run buildDev
+# run production build
 npm run build
+# run test
 npm test
 
+# send test coverage to codeclimate.com
 codeclimate-test-reporter < coverage/lcov.info
+# send test coverage to coveralls.io
+cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
