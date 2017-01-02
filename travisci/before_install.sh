9ff90a3a60f9c5a84b7b69a42b10c94766bb28e0
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-02 16:18:43 +0100

    fix travis config with multiple builds+multiple env variables #42

--- travisci/before_install.sh
+++ travisci/before_install.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-echo "OS is $TRAVIS_OS_NAME"
+echo "Before install - OS is $TRAVIS_OS_NAME"
 
-#echo "Installing global dependencies"
-## export env variables, thanks to https://github.com/travis-ci/travis-ci/issues/7099
-#if [[ $TRAVIS_OS_NAME = 'linux' ]]; then
-#    export CXX=g++-4.8 NODE_ENV=test CI=yes FIREFOX_BIN=$HOME/firefox-latest/firefox PATH=$FIREFOX_BIN:$PATH;
-#else
-#    export NODE_ENV=test CI=yes;
-#fi
+echo "Exporting env variables dependencies"
+# export env variables, thanks to https://github.com/travis-ci/travis-ci/issues/7099
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+    echo "Exporting env variables - OS is $TRAVIS_OS_NAME"
+    export NODE_ENV=test CI=yes;
+    echo "NODE_ENV = $NODE_ENV"
+    echo "CI = $CI"
+    echo "Exporting env variables - done"
+else
+    echo "Exporting env variables - OS is $TRAVIS_OS_NAME"
+    export CXX=g++-4.8 NODE_ENV=test CI=yes FIREFOX_BIN=$HOME/firefox-latest/firefox PATH=$FIREFOX_BIN:$PATH;
+    echo "CXX = $CXX"
+    echo "NODE_ENV = $NODE_ENV"
+    echo "CI = $CI"
+    echo "FIREFOX_BIN = $FIREFOX_BIN"
+    echo "PATH = $PATH"
+    echo "Exporting env variables - done"
+fi
 
 echo "Installing global dependencies"
 # install global dependencies
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    echo "Installing OSX global dependencies"
+    echo "Installing $TRAVIS_OS_NAME global dependencies"
     sudo npm install -g karma-cli
-    sudo npm install -g webpack@2.2.0-rc.2
+    sudo npm install -g webpack@2.2.0-rc.3
     sudo npm install -g typescript@2.0.10
     sudo npm install -g typings
     sudo npm install -g remap-istanbul
@@ -24,9 +35,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     sudo npm install -g codeclimate-test-reporter
     sudo npm install -g istanbul
 else
-    echo "Installing Linux global dependencies"
+    echo "Installing $TRAVIS_OS_NAME global dependencies"
     npm install -g karma-cli
-    npm install -g webpack@2.2.0-rc.2
+    npm install -g webpack@2.2.0-rc.3
     npm install -g typescript@2.0.10
     npm install -g typings
     npm install -g remap-istanbul
--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# run debug builf
+# run debug build
+echo "npm run debug build on $TRAVIS_OS_NAME"
 npm run buildDev
 # run production build
+echo "npm run production build on $TRAVIS_OS_NAME"
 npm run build
 # run test
+echo "npm run test on $TRAVIS_OS_NAME"
 npm test
 
 # send test coverage to codeclimate.com
+echo "npm run codeclimate on $TRAVIS_OS_NAME"
 npm run codeclimate
 # send test coverage to coveralls.io
+echo "npm run coveralls on $TRAVIS_OS_NAME"
 npm run coveralls
