03fe88eb367553d40aee09441cb3776869223858
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-05 00:22:02 +0100

    replaced production with prod also in admin.ts

--- src/admin.ts
+++ src/admin.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AdminModule }  from './admin/admin.module';
 
-if (webpack.ENV === 'production') {
+if (webpack.ENV === 'prod') {
   enableProdMode();
 }
 
