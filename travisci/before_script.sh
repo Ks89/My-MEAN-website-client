2e1ab77f5cb73797fdbb02091585a6930ee1ed1d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-12 07:58:07 +0100

    trying to fix Firefox problems with test #37

---
+++ travisci/before_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Before script - OS is $TRAVIS_OS_NAME"
+
+echo "Setting xvfb"
+# setting xvfb on Linux
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    echo "Do nothing - because OS is $TRAVIS_OS_NAME"
+else
+    "export DISPLAY=:99.0"
+    "sh -e /etc/init.d/xvfb start"
+    sleep 3 # give xvfb some time to start
+fi
