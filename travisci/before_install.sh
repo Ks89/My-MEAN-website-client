059facacec75970f9e3ce9e09c2850f2344acd82
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-12 01:48:15 +0100

    trying to fix Firefox problems with test #37

--- travisci/before_install.sh
+++ travisci/before_install.sh
@@ -23,28 +23,17 @@ echo "Installing global dependencies"
 # install global dependencies
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     echo "Installing $TRAVIS_OS_NAME global dependencies"
-    sudo npm install -g karma-cli
-    sudo npm install -g webpack@2.2.0-rc.3
     sudo npm install -g typescript@2.0.10
     sudo npm install -g typings
-    sudo npm install -g remap-istanbul
     sudo npm install -g webdriver-manager
     sudo npm install -g protractor
-    sudo npm install -g codeclimate-test-reporter
-    sudo npm install -g istanbul
 else
     echo "Installing $TRAVIS_OS_NAME global dependencies"
     # to fix a problem with nodejs 6 on linux
-    npm install -g buffer-shims
-    npm install -g karma-cli
-    npm install -g webpack@2.2.0-rc.3
     npm install -g typescript@2.0.10
     npm install -g typings
-    npm install -g remap-istanbul
     npm install -g webdriver-manager
     npm install -g protractor
-    npm install -g codeclimate-test-reporter
-    npm install -g istanbul
 fi
 
 
