85c72693f04ec754654a453a22f68648eb132833
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-15 04:44:54 +0100

    cleanup webpack config common

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -89,10 +89,10 @@ module.exports = {
         exclude: /node_modules/,
         loaders: ['raw-loader', 'sass-loader']
       },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
+      // {
+      //   test: /\.json$/,
+      //   loader: 'json-loader'
+      // },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "url-loader?limit=10000&mimetype=application/font-woff"
