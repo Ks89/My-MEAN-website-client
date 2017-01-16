4563ea908b2253f75c8665382f06edf4f09b25d5
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-16 01:52:29 +0100

    chrome on travis - cleanup - it's working #37

--- travisci/before_install.sh
+++ travisci/before_install.sh
@@ -18,8 +18,6 @@ else
     echo "CXX = $CXX"
     echo "NODE_ENV = $NODE_ENV"
     echo "CI = $CI"
-#    export CHROME_BIN=/usr/bin/google-chrome;
-#    echo "CHROME_BIN = $CHROME_BIN"
     echo "Exporting env variables - done"
 fi
 
@@ -28,15 +26,12 @@ fi
 echo "Installing global npm dependencies"
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
     echo "Installing $TRAVIS_OS_NAME global npm dependencies"
-    sudo npm install -g webpack@2.2.0-rc.5
     sudo npm install -g typescript@2.0.10
     sudo npm install -g typings
     sudo npm install -g webdriver-manager
     sudo npm install -g protractor
 else
     echo "Installing $TRAVIS_OS_NAME global npm dependencies"
-    # to fix a problem with nodejs 6 on linux
-    npm install -g webpack@2.2.0-rc.5
     npm install -g typescript@2.0.10
     npm install -g typings
     npm install -g webdriver-manager
@@ -45,19 +40,12 @@ fi
 
 
 
-echo "Installing Chrome"
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+    echo "Installing Chrome on $TRAVIS_OS_NAME. For Linux check .travis.yml"
     echo "Installing cask for homebrew"
     brew tap caskroom/cask
     echo "Installing Chrome"
     brew cask install google-chrome
-#else
-    # TODO add chrome for linux
-    # sudo apt-get update
-    # sudo apt-get install -y libappindicator1 fonts-liberation
-    # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    # sudo dpkg -i google-chrome*.deb
-    # export CHROME_BIN=/usr/bin/google-chrome
 fi
 
 
