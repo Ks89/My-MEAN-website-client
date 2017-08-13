6479d519455a26e85c54aa34380c40d0a1303054
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-08-13 20:56:48 +0200

    experiment osx on travisci

--- travisci/before_script.sh
+++ travisci/before_script.sh
@@ -9,4 +9,25 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "DISPLAY is $DISPLAY"
     sh -e /etc/init.d/xvfb start
     sleep 3 # give xvfb some time to start
+else
+    echo "Installing cask on $TRAVIS_OS_NAME"
+    brew tap caskroom/cask
+    echo "Installing chrome on $TRAVIS_OS_NAME"
+    brew cask install google-chrome
+    echo "Installing firefox on $TRAVIS_OS_NAME"
+    brew cask install firefox
+
+    echo "Installing mongodb on $TRAVIS_OS_NAME"
+    brew install mongodb --with-openssl
+    #create a folder for mongodb to prevent an error on mac osx
+    sudo mkdir -p /data/db
+
+    echo "Installing redis on $TRAVIS_OS_NAME"
+    wget http://download.redis.io/redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz
+    cd redis-stable
+    make install
+    cd ..
+    rm -rf redis-stable
+    rm -f redis-stable.tar.gz
 fi
