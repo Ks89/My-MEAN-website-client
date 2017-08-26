789dd03914801978812fd5a7251e34a02d477db9
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-08-26 21:45:14 +0200

    experimenting a new travis config to be able to run e2e tests #21

--- travisci/before_install.sh
+++ travisci/before_install.sh
@@ -2,6 +2,11 @@
 
 echo "Before install - OS is $TRAVIS_OS_NAME"
 
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ------------------- CLIENT SIDE --------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
 echo "Exporting env variables dependencies"
 # export env variables, thanks to https://github.com/travis-ci/travis-ci/issues/7099
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
@@ -29,3 +34,87 @@ else
     # to fix a problem with nodejs 6 on linux
     npm install -g codeclimate-test-reporter
 fi
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+
+
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ------------------- SERVER SIDE --------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+echo "Updating both homebrew, mongodb and redis-server"
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+    echo "Updating homebrew"
+	brew update
+	echo "Installing and starting mongodb"
+    brew install mongodb
+    # create a folder for mongodb to prevent an error on mac osx
+    sudo mkdir -p /data/db
+    brew services start mongodb
+    echo "Installing and starting redis-server"
+    brew install redis
+    brew services start redis
+fi
+
+echo "Exporting env variables dependencies"
+# in this project, all env variables are the same for both linux and osx
+echo "Exporting env variables - OS is $TRAVIS_OS_NAME"
+export NODE_ENV=test
+export CI=yes
+export MONGODB_URI=mongodb://localhost/KS
+export MONGODB_TESTING_URI=mongodb://localhost/test-db
+export JWT_SECRET=faketestjwt
+export TWITTER_CONSUMER_KEY=consumerkey
+export TWITTER_CONSUMER_SECRET=consumersecret
+export TWITTER_CALLBACK_URL=http://127.0.0.1:3300/api/auth/twitter/callback
+export TWITTER_PROFILE_URL=https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true
+export FACEBOOK_APP_ID=appid
+export FACEBOOK_APP_SECRET=appsecret
+export FACEBOOK_CALLBACK_URL=http://localhost:3300/api/auth/facebook/callback
+export GOOGLE_CLIENT_ID=clientid
+export GOOGLE_CLIENT_SECRET=clientsecret
+export GOOGLE_CALLBACK_URL=http://localhost:3300/api/auth/google/callback
+export GITHUB_CLIENT_ID=clientid
+export GITHUB_CLIENT_SECRET=clientsecret
+export GITHUB_CALLBACK_URL=http://localhost:3300/api/auth/github/callback
+export LINKEDIN_CLIENT_ID=clientid
+export LINKEDIN_CLIENT_SECRET=clientsecret
+export LINKEDIN_CALLBACK_URL=http://localhost:3300/api/auth/linkedin/callback
+export USER_EMAIL=fake@fake.it
+export PASS_EMAIL=fakepasswordemail
+export RECAPTCHA_PUBLIC=recaptchapublic
+export RECAPTCHA_SECRET=recaptchasecret
+echo "NODE_ENV = $NODE_ENV"
+echo "CI = $CI"
+echo "MONGODB_URI = $MONGODB_URI"
+echo "MONGODB_TESTING_URI = $MONGODB_TESTING_URI"
+echo "JWT_SECRET = $JWT_SECRET"
+echo "TWITTER_CONSUMER_KEY = $TWITTER_CONSUMER_KEY"
+echo "TWITTER_CONSUMER_SECRET = $TWITTER_CONSUMER_SECRET"
+echo "TWITTER_CALLBACK_URL = $TWITTER_CALLBACK_URL"
+echo "TWITTER_PROFILE_URL = $TWITTER_PROFILE_URL"
+echo "FACEBOOK_APP_ID = $FACEBOOK_APP_ID"
+echo "FACEBOOK_APP_SECRET = $FACEBOOK_APP_SECRET"
+echo "FACEBOOK_CALLBACK_URL = $FACEBOOK_CALLBACK_URL"
+echo "GOOGLE_CLIENT_ID = $GOOGLE_CLIENT_ID"
+echo "GOOGLE_CLIENT_SECRET = $GOOGLE_CLIENT_SECRET"
+echo "GOOGLE_CALLBACK_URL = $GOOGLE_CALLBACK_URL"
+echo "GITHUB_CLIENT_ID = $GITHUB_CLIENT_ID"
+echo "GITHUB_CLIENT_SECRET = $GITHUB_CLIENT_SECRET"
+echo "GITHUB_CALLBACK_URL = $GITHUB_CALLBACK_URL"
+echo "LINKEDIN_CLIENT_ID = $LINKEDIN_CLIENT_ID"
+echo "LINKEDIN_CLIENT_SECRET = $LINKEDIN_CLIENT_SECRET"
+echo "LINKEDIN_CALLBACK_URL = $LINKEDIN_CALLBACK_URL"
+echo "USER_EMAIL = $USER_EMAIL"
+echo "PASS_EMAIL = $PASS_EMAIL"
+echo "RECAPTCHA_PUBLIC = $RECAPTCHA_PUBLIC"
+echo "RECAPTCHA_SECRET = $RECAPTCHA_SECRET"
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
--- travisci/before_script.sh
+++ travisci/before_script.sh
@@ -2,6 +2,12 @@
 
 echo "Before script - OS is $TRAVIS_OS_NAME"
 
+
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ------------------- CLIENT SIDE --------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
 echo "Setting xvfb based on TRAVIS_OS_NAME"
 # setting xvfb on Linux https://docs.travis-ci.com/user/gui-and-headless-browsers/#Using-xvfb-to-Run-Tests-That-Require-a-GUI
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
@@ -17,3 +23,43 @@ else
     echo "Installing chrome on $TRAVIS_OS_NAME"
     brew cask install google-chrome
 fi
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+
+
+
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ------------------- SERVER SIDE --------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+echo "Setting xvfb based on TRAVIS_OS_NAME"
+# setting xvfb on Linux https://docs.travis-ci.com/user/gui-and-headless-browsers/#Using-xvfb-to-Run-Tests-That-Require-a-GUI
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    echo "Before script on $TRAVIS_OS_NAME"
+    echo "$TRAVIS_OS_NAME doesn't require a custom configuration"
+else
+    echo "Installing mongodb on $TRAVIS_OS_NAME"
+    brew install mongodb --with-openssl
+    #create a folder for mongodb to prevent an error on mac osx
+    sudo mkdir -p /data/db
+#    brew services start mongodb
+
+    echo "Installing redis on $TRAVIS_OS_NAME"
+    wget http://download.redis.io/redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz
+    cd redis-stable
+    make install
+    cd ..
+    rm -rf redis-stable
+    rm -f redis-stable.tar.gz
+fi
+
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
+# ----------------------------------------------------
