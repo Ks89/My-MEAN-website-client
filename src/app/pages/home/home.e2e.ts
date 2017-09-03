239ad472df6489e128d68a376b1a4836425f429d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-09-04 00:17:08 +0200

    e2e testing with protractor #21

--- src/app/pages/home/home.e2e.ts
+++ src/app/pages/home/home.e2e.ts
@@ -14,16 +14,48 @@
  * limitations under the License.
  */
 
-import { browser, by, element, ElementFinder } from 'protractor';
+import { browser, by, element, ElementArrayFinder } from 'protractor';
 
 describe('App', () => {
 
-  beforeAll(async () => {
-    await browser.get('/');
+  beforeAll( () => {
+    browser.get('/');
   });
 
-  it('should have a page header', async () => {
-    let title: ElementFinder = element(by.id('title'));
-    expect(title.getText()).toEqual('KS Welcome');
+  it('should display the homepage with projects', () => {
+    let subject: any =  browser.getTitle();
+    expect(subject).toEqual('My MEAN Website');
+
+    let titleText: any =  element(by.id('title')).getText();
+    expect(titleText).toEqual('KS Welcome');
+
+    let cards = element.all(by.css('.card-body')).all(by.css('.card-title.text-center'));
+    expect(cards.count()).toEqual(3);
+    expect(cards.getText()).toEqual(['BYAManager', 'SPF', 'Superapp']);
+
+    let cardsDescription = element.all(by.css('.card-body')).all(by.css('.card-text'));
+    expect(cardsDescription.count()).toEqual(3);
+    expect(cardsDescription.getText()).toEqual(['sfsfasf', 'sfsfasf', 'sfsfasf']);
+
+    let featuretteHeading = element.all(by.css('.featurette-heading'));
+    expect(featuretteHeading.count()).toEqual(3);
+    expect(featuretteHeading.getText()).toEqual([`BYAManager It'll blow your mind.`, `SPF It'll blow your mind.`, `Superapp It'll blow your mind.`]);
+
+    let featuretteDescription = element.all(by.css('p.lead'));
+    expect(featuretteDescription.count()).toEqual(3);
+    expect(featuretteDescription.getText()).toEqual(['dasdasdas', 'dasdasdas', 'dasdasdas']);
+
+    let cookieConsent = element.all(by.css('cookie-law-el')).all(by.css('.cookie-law-wrapper.ng-trigger.ng-trigger-transition')).all(by.css('span'));
+    expect(cookieConsent.count()).toEqual(2);
+    expect(cookieConsent.getText()).toEqual(['', `By continuing to browse the site, you're agreeing to our use of cookies.`]);
+
+    let cookieConsentClose = element.all(by.css('cookie-law-el')).all(by.css('.dismiss'));
+    expect(cookieConsentClose.count()).toEqual(1);
+    cookieConsentClose.get(0).click();
+    // TODO click this button
+
+    let copyright = element.all(by.css('footer')).all(by.css('p'));
+    expect(copyright.count()).toEqual(1);
+    expect(copyright.getText()).toEqual(['Copyright © Stefano Cappa 2015-2016']);
   });
 });
