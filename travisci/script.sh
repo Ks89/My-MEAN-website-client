2d93404a4952a791b1746649f51bfbf2ff82ce9d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-09-08 22:14:25 +0200

    e2e tests with protractor #21 - desperate experiment for travisci

--- travisci/script.sh
+++ travisci/script.sh
@@ -2,11 +2,11 @@
 
 # run debug build
 echo "npm run debug build on $TRAVIS_OS_NAME"
-npm run build:dev
+#npm run build:dev
 
 # run production build
 echo "npm run production build on $TRAVIS_OS_NAME"
-npm run build:prod
+#npm run build:prod
 
 # clean before the real production build
 npm run clean
@@ -17,7 +17,7 @@ npm run build:prod:aot
 
 # run unit test
 echo "npm run test on $TRAVIS_OS_NAME"
-npm test
+#npm test
 
 # run e2e test (requires server-side up and running)
 echo "dropping db collections"
@@ -31,11 +31,15 @@ sleep 5
 # start
 export FRONT_END_PATH=../My-MEAN-website-client/dist
 echo "FRONT_END_PATH is $FRONT_END_PATH"
+export BYPASS_CI=yes
+echo "BYPASS_CI is $BYPASS_CI"
+export NODE_ENV=development
+echo "NODE_ENV is $NODE_ENV"
 
 cd ../My-MEAN-website-server
 # npm --prefix ../My-MEAN-website-server/ run prod:start
 # pm2 start bin/www -i 4 --env development  # I should change this with a custom value, for instance "e2e" or "ci"
-BYPASS_CI=yes NODE_ENV=development npm start NODE_ENV=development npm start # CI==='yes' by default and you cannot change it
+npm start NODE_ENV=development npm start # CI==='yes' by default and you cannot change it
 cd ../My-MEAN-website-client
 
 sleep 5
