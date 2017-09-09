57eafc3cc96b5743f2a56f553cf03cafbcc5b6b3
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-09-09 14:27:21 +0200

    e2e tests with protractor #21 - finally working on travisci

--- travisci/script.sh
+++ travisci/script.sh
@@ -20,15 +20,11 @@ echo "npm run test on $TRAVIS_OS_NAME"
 npm test
 
 # run e2e test (requires server-side up and running)
-echo "dropping db collections"
-mongo test-db --eval 'db.projects.drop()'
-mongo KS --eval "db.projects.drop()"
-
+echo "dropping db collections for test-db"
 mongo test-db --eval "db.getCollectionNames()"
-mongo KS --eval "db.getCollectionNames()"
+mongo test-db --eval 'db.projects.drop()'
 
-echo "filling db with data"
-mongorestore -d KS -c projects --dir=./db-dump-e2e/KS/projects.bson --maintainInsertionOrder
+echo "filling test-db with data"
 mongorestore -d test-db -c projects --dir=./db-dump-e2e/KS/projects.bson --maintainInsertionOrder
 
 sleep 5
@@ -41,8 +37,7 @@ echo "BYPASS_CI is $BYPASS_CI"
 
 cd ../My-MEAN-website-server
 # npm --prefix ../My-MEAN-website-server/ run prod:start
-pm2 start bin/www -i 4 --env development  # I should change this with a custom value, for instance "e2e" or "ci"
-# BYPASS_CI=yes NODE_ENV=development npm start # CI==='yes' by default and you cannot change it
+pm2 start bin/www -i 4 --env development  # FIXME I should change this with a custom value instead of "development", for instance "e2e" or "ci"
 cd ../My-MEAN-website-client
 
 
