7a0ac16fca8fb5db1a0ab3543107cdc68090d97f
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-10-04 01:14:21 +0200

    fix for CIs #21

--- travisci/script.sh
+++ travisci/script.sh
@@ -21,19 +21,8 @@ npm test
 
 # run e2e test (requires a db with some data and the server-side up and running)
 echo "dropping db collections for test-db"
-mongo test-db --eval "db.getCollectionNames()"
-mongo test-db --eval 'db.projects.drop()'
-mongo test-db --eval 'db.users.drop()'
-
-echo "filling test-db with data"
-mongorestore -d test-db -c projects --dir=./db-dump-e2e/KS/projects.bson --maintainInsertionOrder
-mongorestore -d test-db -c users --dir=./db-dump-e2e/KS/users.bson --maintainInsertionOrder
-
-echo "updating activation expire date"
-mongo test-db --eval 'db.users.update({ "local.email":"activate@fake-mmw.com"}, { $set: { "local.activateAccountExpires": new Date((new Date()).getTime() + 10 * 24 * 60 * 60 * 1000)} })'
-
-echo "updating reset expire date"
-mongo test-db --eval 'db.users.update({ "local.email":"reset@fake-mmw.com"}, { $set: { "local.resetPasswordExpires": new Date((new Date()).getTime() + 10 * 24 * 60 * 60 * 1000)} })'
+npm run init-db
+npm run build:prod:aot #again because it is used by e2e testing
 
 sleep 5
 
