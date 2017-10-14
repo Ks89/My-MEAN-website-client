cd97985dbcdbe76c7faf43020942f14840d55311
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-10-14 17:14:00 +0200

    (fix) experimental fix for travisci to be able to run mongorestore

--- .circleci/install-mongodb.sh
+++ .circleci/install-mongodb.sh
@@ -4,12 +4,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A1
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/testing multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 echo "updating apt"
 sudo apt-get update
-
-sudo apt-get purge -y libssl1.0.0
-sudo apt-get purge -y 1.0.1t-1+deb8u6
-
-sudo apt-get install -y libssl
-
 echo "installing mongodb"
 sudo apt-get install -y mongodb-org-shell
 echo "mongodb install script comleted"
